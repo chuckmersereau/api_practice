20e54d615034a735730961fbca55b4183f1d5254
LaelWatkins <laelwatkins@gmail.com>
2014-10-22 14:52:25 -0400

    Changes for recurring gift recommendations

--- app/models/obiee.rb
+++ app/models/obiee.rb
@@ -11,11 +11,7 @@ class Obiee
 
   def report_sql(session_id,path,presentation_var,desig)
 
-    get_report_client = Savon.client do
-      endpoint "http://plobia08.ccci.org:9704/analytics-ws/saw.dll?SoapImpl=reportService"
-      namespace "urn://oracle.bi.webservices/v7"
-    end
-
+    get_report_client = get_client("http://plobia08.ccci.org:9704/analytics-ws/saw.dll?SoapImpl=reportService")
     report_params = { reportRef: {reportPath: path},
                       reportParams: {filterExpressions: '',
                                      variables: {
@@ -25,18 +21,13 @@ class Obiee
                       },
                       sessionID: session_id}
 
-
     report_message = get_report_client.call( :generateReportSQL, message: report_params )
     report_message.body[:generate_report_sql_result][:return]
   end
 
   def report_results(session_id, report_sql)
 
-    run_report_client = Savon.client do
-      endpoint "http://plobia08.ccci.org:9704/analytics-ws/saw.dll?SoapImpl=xmlViewService"
-      namespace "urn://oracle.bi.webservices/v7"
-    end
-
+    run_report_client = get_client("http://plobia08.ccci.org:9704/analytics-ws/saw.dll?SoapImpl=xmlViewService")
     run_params = {sql: report_sql,
                   outputFormat: 'SAWRowsetSchemaAndData',
                   executionOptions:
@@ -48,7 +39,19 @@ class Obiee
                   sessionID: session_id}
 
     returned_results = run_report_client.call(:executeSQLQuery, message: run_params)
+    #run_report_client.call( :logoff, message: session_id)  for some reason this call returns with the expired/invalid session exception
     returned_results.body[:execute_sql_query_result][:return][:rowset]
 
   end
+
+  def get_client(url)
+
+  Savon.client do
+      endpoint url
+      namespace "urn://oracle.bi.webservices/v7"
+    end
+
+  end
+
+
 end
