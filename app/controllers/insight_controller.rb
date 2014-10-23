8d3491fc7c6c13b5bb13c27385fb8c0fb80b4a27
LaelWatkins <laelwatkins@gmail.com>
2014-10-23 18:27:25 -0400

    Changes for recurring gift recommendations

--- app/controllers/insight_controller.rb
+++ app/controllers/insight_controller.rb
@@ -1,3 +1,4 @@
+require 'rexml/document'
 
 class InsightController < ApplicationController
 
@@ -11,12 +12,16 @@ class InsightController < ApplicationController
     sql = insight.report_sql(session_id,
                              '/shared/Insight/Siebel Recurring Monthly/Recurring Gift Recommendations',
                              'mpdxRecurrDesig',
-                             Person::RelayAccount.where(person_id: current_user.id ).pluck('designation')[0])
+                             '0124650')
+                             #Person::RelayAccount.where(person_id: current_user.id ).pluck('designation')[0])
 
     result_columns =  insight.report_results(session_id,sql)
+    xmldoc = REXML::Document.new(result_columns)
 
-    @desig_accnt = result_columns
+    @col_headers = xmldoc.elements.to_a('//xsd:element').map { |el| el.attributes['saw-sql:columnHeading'].to_s}
+    @col_values = xmldoc.elements[ '//Row' ].next_element.to_a
 
+    @result_columns = result_columns
     end
 
 end
