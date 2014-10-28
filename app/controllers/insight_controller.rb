800aad19f83a29accebc27bd1144f7835ddab440
LaelWatkins <laelwatkins@gmail.com>
2014-10-28 17:56:10 -0400

    Changes for recurring gift recommendations

--- app/controllers/insight_controller.rb
+++ app/controllers/insight_controller.rb
@@ -1,6 +1,8 @@
 require 'rexml/document'
 
 class InsightController < ApplicationController
+  before_action :find_contact
+  before_action :find_donor_accounts
 
   def index
     @page_title = _('Insight')
@@ -12,7 +14,7 @@ class InsightController < ApplicationController
     sql = insight.report_sql(session_id,
                              '/shared/Insight/Siebel Recurring Monthly/Recurring Gift Recommendations',
                              'mpdxRecurrDesig',
-                             Person::RelayAccount.where(person_id: current_user.id ).pluck('designation')[0])
+                            Person::RelayAccount.where(person_id: current_user.id ).pluck('designation')[0])
 
     xsd_results =  insight.report_results(session_id,sql)
     dom = Hash.from_trusted_xml(xsd_results).deep_symbolize_keys
@@ -20,6 +22,26 @@ class InsightController < ApplicationController
     @xsd_results = dom[:rowset][:Row]
     @acct = current_account_list
 
+  end
+
+  def find_contact
+    @contact = current_account_list.contacts.where(id: params[:contact_id]).first if params[:contact_id]
+  end
+
+  def find_donor_accounts
+    @donor_accounts = []
+    if @contact
+      @contact.donor_accounts.each do |da|
+        @donor_accounts << [ "(#{da.account_number})", da.id ]
+      end
+    else
+      current_account_list.contacts.active.joins(:donor_accounts).includes(:donor_accounts).each do |c|
+        c.donor_accounts.each do |da|
+          @donor_accounts << ["(#{da.account_number})", da.id]
+        end
+      end
+      @donor_accounts.sort_by!(&:first)
     end
+  end
 
 end
