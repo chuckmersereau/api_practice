a55770f6b3dae2f147d11c9c8e64ab39592fabdf
LaelWatkins <laelwatkins@gmail.com>
2014-10-31 17:41:33 -0400

    recurring recommendations changed the params passed to the report from two string fields to a hash so multiple  variable key value combos can be used for future reports.

--- app/controllers/insight_controller.rb
+++ app/controllers/insight_controller.rb
@@ -9,10 +9,11 @@ class InsightController < ApplicationController
     insight = Obiee.new
     session_id = insight.auth_client
 
+
+    vars = { name: 'mpdxRecurrDesig',value: Person::RelayAccount.where(person_id: current_user.id ).pluck('designation')[0] }
     sql = insight.report_sql(session_id,
                              '/shared/Insight/Siebel Recurring Monthly/Recurring Gift Recommendations',
-                             'mpdxRecurrDesig',
-                            Person::RelayAccount.where(person_id: current_user.id ).pluck('designation')[0])
+                            vars)
 
     xsd_results =  insight.report_results(session_id,sql)
     dom = Hash.from_trusted_xml(xsd_results).deep_symbolize_keys
