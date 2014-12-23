dda1af2a9759b04a2d31c294b5cb8e78fd7de2b9
LaelWatkins <laelwatkins@gmail.com>
2014-12-23 17:13:37 -0500

    - specs

--- spec/models/insight_analyses_spec.rb
+++ spec/models/insight_analyses_spec.rb
@@ -92,5 +92,43 @@ FETCH FIRST 10000000 ROWS ONLY',
   end
 
 
+  it 'fails with no desig' do
+
+    creds = {name: APP_CONFIG['obiee_key'], password: APP_CONFIG['obiee_secret'] }
+    fixture = File.read('spec/fixtures/obiee_auth_client.xml')
+
+
+    if savon.expects(:logon).with(message: creds).returns(fixture)
+      rpt_sql_fixture = File.read('spec/fixtures/obiee_report_sql.xml')
+      report_params = { reportRef: {reportPath: '/shared/Insight/Siebel Recurring Monthly/Recurring Gift Recommendations'},
+                        reportParams: {filterExpressions: '',
+                                       variables: {:name=>"mpdxRecurrDesig", :value=>"2716653"}
+                        },
+                        sessionID: 'sessionid22091522cru'}
+
+      if savon.expects(:generateReportSQL).with(message: report_params ).returns(rpt_sql_fixture)
+        results_fixture = File.read('spec/fixtures/obiee_report_results.xml')
+        run_params = {sql: 'SELECT
+   0 s_0,
+   "CCCi Transaction Analytics"."- Designation"."Designation Name" s_1
+FROM "CCCi Transaction Analytics"
+ORDER BY 1, 2 ASC NULLS LAST
+FETCH FIRST 10000000 ROWS ONLY',
+                      outputFormat: 'SAWRowsetSchemaAndData',
+                      executionOptions:
+                          {async: '',
+                           maxRowsPerPage: -1,
+                           refresh: true,
+                           presentationInfo: true,
+                           type: ''},
+                      sessionID: 'sessionid22091522cru'}
+        savon.expects(:executeSQLQuery).with(message: run_params ).returns(results_fixture)
+
+      end
+    end
+
+    #unknown desig
+    expect{ InsightAnalyses.new.increase_recommendation_analysis('')}.to raise_error( Savon::ExpectationError )
+  end
 
 end
