80d37d3a83ebfd8c1e4ac792c342f1ee27c90c27
LaelWatkins <laelwatkins@gmail.com>
2015-01-19 17:48:23 -0500

    changes to the model, spec, controller and views. got rid of deep symbolize keys.  it was causing expected hash outputs to be arrays.

--- app/models/insight_analyses.rb
+++ app/models/insight_analyses.rb
@@ -8,23 +8,16 @@ class InsightAnalyses
     sql = insight.report_sql(session_id,
                              '/shared/Insight/Siebel Recurring Monthly/Recurring Gift Recommendations',
                              vars)
-   Hash.from_trusted_xml(insight.report_results(session_id,sql)).deep_symbolize_keys
+  Hash.from_xml(insight.report_results(session_id,sql))
   end
 
   def increase_recommendation_contacts(designation_number)
 
-    recommends = increase_recommendation_analysis( designation_number)[:rowset][:Row]
+    recommends = increase_recommendation_analysis( designation_number)
     contacts_list = Array.new
-    puts "say something"
-puts recommends.inspect
-
     unless recommends.empty?
-      recommends.each{ |c,val|
-        puts "say hi"
-        puts c.class
-        if c.include?(:Column8)
-          contacts_list.push(c[:Column8].to_s)
-        end
+      recommends["rowset"]["Row"].each{ |c,val|
+          contacts_list.push(c["Column8"])
       }
     end
     contacts_list
