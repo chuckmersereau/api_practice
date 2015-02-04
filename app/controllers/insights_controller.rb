a6426c5d7eac2bda0cc95e6c5eeb8bc371f14775
LaelWatkins <laelwatkins@gmail.com>
2015-02-04 11:20:47 -0500

    fixed condition to display contacts and removed test designation number

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -2,8 +2,7 @@ class InsightsController < ApplicationController
 
   def index
     @page_title = _('Insights')
-    #@recommnds = InsightAnalyses.new.increase_recommendation_analysis( current_account_list.designation_accounts.pluck(:designation_number).first)["rowset"]["Row"]
-    @recommnds = InsightAnalyses.new.increase_recommendation_analysis( '0124650')["rowset"]["Row"]
+    @recommnds = InsightAnalyses.new.increase_recommendation_analysis( current_account_list.designation_accounts.pluck(:designation_number).first)["rowset"]["Row"]
     @recurring_recommnds = RecurringRecommendationResults
    end
 
