5ecfd3cf848adfed3a0b0ef41734b383e8794ef3
Justin Sabelko <justin.sabelko@cru.org>
2015-02-04 10:25:00 -0600

    Fix and optimize migration

--- db/migrate/20150202221959_add_master_address_id_constraint.rb
+++ db/migrate/20150202221959_add_master_address_id_constraint.rb
@@ -1,7 +1,8 @@
 class AddMasterAddressIdConstraint < ActiveRecord::Migration
   def change
-    Address.where(master_address_id: nil).each do |address|
+    Address.where(master_address_id: nil).find_each(batch_size: 500) do |address|
       address.find_or_create_master_address
+      address.save!
     end
 
     change_column_null :addresses, :master_address_id, false
