7a6b5fb4ce81a10a5fb8cef8939e482b4b6397d1
LaelWatkins <laelwatkins@gmail.com>
2015-02-17 10:59:23 -0500

    Sync with master before merge back into master.

--- app/models/recurring_recommendation_results.rb
+++ app/models/recurring_recommendation_results.rb
@@ -2,5 +2,4 @@ class RecurringRecommendationResults < ActiveRecord::Base
   validates :result, presence: true
   validates :contact_id, presence: true
   validates :account_list_id, presence: true
-  validates :contact_id, :account_list_id, uniqueness: true
 end
