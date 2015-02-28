3bec1c44ebfd8a16cb7a26f12719519a8fbae755
draffensperger <draff8660@gmail.com>
2015-02-28 09:40:44 -0500

    Change donation fields to Postgres money type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227212023) do
+ActiveRecord::Schema.define(version: 20150228143706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
