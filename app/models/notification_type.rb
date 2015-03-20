e64394fba16c2df0bfc0c33e49aacf834e387e28
draffensperger <draff8660@gmail.com>
2015-03-20 14:49:16 -0400

    Wrap new notification checks in $rollout

--- app/models/notification_type.rb
+++ app/models/notification_type.rb
@@ -12,6 +12,10 @@ class NotificationType < ActiveRecord::Base
   def self.check_all(account_list)
     contacts = {}
     types.each do |type|
+      unless $rollout.active?(:new_notifications, account_list)
+        next if type.in?(['NotificationType::LargerGift', 'NotificationType::LongTimeFrameGift',
+                         'NotificationType::RecontinuingGift', 'NotificationType::SmallerGift'])
+      end
       type_instance = type.constantize.first
       actions = account_list.notification_preferences.find_by_notification_type_id(type_instance.id).try(:actions)
       next unless (Array.wrap(actions) & NotificationPreference.default_actions).present?
