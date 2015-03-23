5c74446bc0d6f1d00241cd9e5f1ef7aa7b9d2b4d
draffensperger <draff8660@gmail.com>
2015-03-23 10:53:09 -0400

    Add check for retried or running dup Sidekiq job

--- app/workers/mail_chimp_worker.rb
+++ app/workers/mail_chimp_worker.rb
@@ -1,9 +1,14 @@
+require 'job_duplicate_checker'
+
 class MailChimpWorker
   include Sidekiq::Worker
+  include JobDuplicateChecker
 
   sidekiq_options backtrace: true, unique: true
 
   def perform(klass_str, id, method, *args)
+    return if duplicate_job?(klass_str, id, method, *args)
+
     klass = klass_str.constantize
     begin
       object = id ? klass.find(id) : klass
