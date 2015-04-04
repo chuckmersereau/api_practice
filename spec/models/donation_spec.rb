21453d3a0c02e32ede69be093bf0c55f748973c0
Spencer Oberstadt <soberstadt@gmail.com>
2015-04-04 17:57:30 -0500

    just include the date when talking about when donation was given

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Donation do
+  let(:da) { create(:designation_account) }
+  let(:donor_account) { create(:donor_account) }
+  let(:donation) { create(:donation) }
+
+  context '#localized_date' do
+    it 'is just date' do
+      expect(donation.localized_date).to_not be_blank
+    end
+  end
 end
