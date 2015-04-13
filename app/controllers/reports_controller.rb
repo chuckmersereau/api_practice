b1715462274dceb7a0adf42f5296624332b65d46
Spencer Oberstadt <soberstadt@gmail.com>
2015-04-13 15:40:42 -0500

    only inlcude financial partners in contrib report total

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -76,7 +76,7 @@ class ReportsController < ApplicationController
     @total_pledges = 0.0
     @total_average = 0.0
     @donations.each do |_key, row|
-      if row[:pledge_amount].present?
+      if row[:pledge_amount].present? && row[:status] == 'Partner - Financial'
         @total_pledges += row[:pledge_amount] / (row[:pledge_frequency] || 1)
       end
 
