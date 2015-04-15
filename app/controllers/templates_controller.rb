45bd8035510b1b200760ad8a78c98302a4c14d42
Josh Starcher <josh.starcher@gmail.com>
2015-04-14 19:11:11 -0700

    Cache angular templates client side for 15 mins.
    The only concern is with translations. If you switch languages, your
    angular templates won't pick up the new language for 15 mins or until
    you do a full refresh. The infrequency with which that happens compared
    to the value for all the other users seems worth it.

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,5 +1,6 @@
 class TemplatesController < ApplicationController
   def template
+    expires_in 15.minutes, public: true
     render template: 'angular/' + params[:path], layout: false
   end
 end
