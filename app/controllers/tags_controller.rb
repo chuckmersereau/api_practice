6892ff7237788f3c23df602e9155402d5222d6e4
chencuicru <chen.cui@cru.org>
2015-04-17 15:13:23 -0400

    mpdx-179 v1

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,11 +9,26 @@ class TagsController < ApplicationController
   end
 
   def destroy
+    p params
     return if params[:remove_tag_name].blank?
-    contacts = current_account_list.contacts.where(id: params[:remove_tag_contact_ids].split(','))
-    contacts.each do |c|
-      c.tag_list.remove(params[:remove_tag_name].downcase)
-      c.save
+    if params[:all_contacts] == '1'
+      contacts = current_account_list.contacts
+      contacts.each do |c|
+        c.tag_list.remove(params[:remove_tag_name].downcase)
+        c.save
+      end
+    elsif params[:all_tasks] == '1'
+      tasks = current_account_list.tasks
+      tasks.each do |t|
+        t.tag_list.remove(params[:remove_tag_name].downcase)
+        t.save
+      end
+    else
+      contacts = current_account_list.contacts.where(id: params[:remove_tag_contact_ids].split(','))
+      contacts.each do |c|
+        c.tag_list.remove(params[:remove_tag_name].downcase)
+        c.save
+      end
     end
   end
 end
