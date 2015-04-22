f96a9a7ac7c1a70042a9a74685b59e3bbc95059e
chencuicru <chen.cui@cru.org>
2015-04-22 15:59:29 -0400

    Merge branch 'mpdx-179' into dev
    Conflicts:
    	app/views/application/_sidebar_tags.html.erb

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -1,11 +1,11 @@
 require Rails.root.join('config', 'initializers', 'redis').to_s
 $rollout = Rollout.new(Redis.current)
 
+$rollout.activate_group(:insights, :owners)
+
 $rollout.define_group(:testers) do |account_list|
   account_list.tester == true
 end
-
-$rollout.activate_group(:insights, :owners)
 
 $rollout.define_group(:owners) do |account_list|
   account_list.owner == true
