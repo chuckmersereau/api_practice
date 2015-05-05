2ddeef9123128a761a6258d58d315f63e859b346
adammeyer <adam.meyer@cru.org>
2015-05-05 16:32:30 -0400

    Update contact obj

--- app/assets/javascripts/angular/directives/replaceAvatar.js
+++ app/assets/javascripts/angular/directives/replaceAvatar.js
@@ -2,19 +2,13 @@ angular.module('mpdxApp')
     .directive('replaceAvatar', function () {
         return {
             restrict: 'A',
-            template: '',
             controller: function ($scope, $modal, api) {
 
-              $scope.replaceFacebookLink = function(contactId, personId, url){
-                var contactObj;
-                api.call('get', 'contacts/' + contactId, null, function(data){
-                  contactObj = data;
-                });
-
+              $scope.replaceFacebookLink = function(contactId, personId){
                 $modal.open({
                   templateUrl: '/templates/contacts/editFacebookUrl.html',
-                  controller: function($scope, $modalInstance, facebookUrl){
-                    $scope.facebookUrl = facebookUrl;
+                  controller: function($scope, $modalInstance){
+                    $scope.facebookUrl = '';
 
                     $scope.cancel = function () {
                       $modalInstance.dismiss();
@@ -23,24 +17,21 @@ angular.module('mpdxApp')
                     $scope.save = function () {
                       $modalInstance.close($scope.facebookUrl);
                     };
-                  },
-                  resolve: {
-                    facebookUrl: function () {
-                      return url;
-                    }
                   }
                 }).result.then(function (facebookUrl) {
-                  obj = {
+                  var obj = {
                     contact: {
                       people_attributes: [{
-                        id: contactObj.people[0]['id'],
+                        id: personId,
                         facebook_accounts_attributes: {
-                          1430848487081: { url: facebookUrl }
+                          0: { url: facebookUrl }
                         }
                       }]
                     }
-                  }
-                  api.call('put', 'contacts/' + contactId + '?account_list_id=' + window.current_account_list_id, obj);
+                  };
+                  api.call('put', 'contacts/' + contactId + '?account_list_id=' + window.current_account_list_id, obj, function(){
+                    location.reload();
+                  });
                 });
               };
             }
