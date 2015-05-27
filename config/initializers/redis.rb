ff039452193bac4a7b20f2a6c40591df2816a50e
Josh Starcher <josh.starcher@gmail.com>
2015-05-27 15:06:19 -0700

    Fix redis.yml

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,7 +4,7 @@ require 'redis/namespace'
 rails_root = ENV['RAILS_ROOT'] || Rails.root.to_s
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-redis_config = YAML.load_file(rails_root + '/config/redis.yml')
+redis_config = YAML.load(ERB.new(File.read(Rails.root.join('config', 'redis.yml').to_s)).result)
 host, port = redis_config[rails_env].split(':')
 Redis.current = Redis::Namespace.new("MPDX:#{rails_env}", redis: Redis.new(host: host, port: port))
 Mpdx::Application.configure do
