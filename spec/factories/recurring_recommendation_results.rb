df0fd4edca7f6312b8073d5ee403cb6822743ae4
draffensperger <draff8660@gmail.com>
2015-05-29 08:30:38 -0400

    Fix account list merge to include newer tables

--- spec/factories/recurring_recommendation_results.rb
+++ spec/factories/recurring_recommendation_results.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :recurring_recommendation_results do
+  factory :recurring_recommendation_result do
     contact_id '1234561'
     account_list_id '789012'
     result 'test result message'
