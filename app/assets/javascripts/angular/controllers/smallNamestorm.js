35a29d14c7e0b56b72f2cca3b8b92bd27169952f
Spencer Oberstadt <soberstadt@gmail.com>
2015-06-16 09:58:01 -0600

    set default status in new contact on welcome panel

--- app/assets/javascripts/angular/controllers/smallNamestorm.js
+++ app/assets/javascripts/angular/controllers/smallNamestorm.js
@@ -7,12 +7,16 @@ angular.module('mpdxApp').controller('smallNamestorm', function (api) {
                           "Partner - Financial","Partner - Special","Partner - Pray",
                           "Not Interested","Unresponsive","Never Ask","Research Abandoned",
                           "Expired Referral"];
+  this.contactStatuses = _.map(this.contactStatuses, function(status) {
+    return { value: status, text: __(status) };
+  });
 
   this.addContact = function() {
     var scope = this;
     this.apiBusy = true;
     this.error = '';
-    api.post('contacts/?account_list_id=' + window.current_account_list_id, { contact: { name: this.newContactName } })
+    api.post('contacts/?account_list_id=' + window.current_account_list_id,
+             { contact: { name: this.newContactName, status: this.newContactStatus } })
        .success(function(response) {
         scope.contacts.push({id: response.contact.id, name: response.contact.name});
         scope.newContactName = '';
