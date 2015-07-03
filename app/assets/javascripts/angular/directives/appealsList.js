c7a430cc44658058b4ae3d17ce445c2c88b9f40c
Spencer Oberstadt <soberstadt@gmail.com>
2015-07-03 15:54:21 -0600

    DRY up pledgeFreq to str angular code

--- app/assets/javascripts/angular/directives/appealsList.js
+++ app/assets/javascripts/angular/directives/appealsList.js
@@ -77,29 +77,6 @@ angular.module('mpdxApp')
                               return '';
                             };
 
-                          $scope.pledgeFrequencyStr = function(pledgeFrequency){
-                            switch(pledgeFrequency) {
-                              case '0.23076923076923':
-                                return 'Weekly';
-                              case '0.46153846153846':
-                                return 'Fortnightly';
-                              case '1':
-                                return 'Monthly';
-                              case '2':
-                                return 'Bi-Monthly';
-                              case '3':
-                                return 'Quarterly';
-                              case '4':
-                                return 'Quad-Monthly';
-                              case '6':
-                                return 'Semi-Annual';
-                              case '12':
-                                return 'Annual';
-                              case '24':
-                                return 'Biennial';
-                            }
-                          };
-
                             $scope.addContact = function(id){
                                 if(!id){ return; }
                                 if(_.contains($scope.appeal.contacts, id)){
--- app/assets/javascripts/angular/directives/contact.js
+++ app/assets/javascripts/angular/directives/contact.js
@@ -42,29 +42,6 @@ angular.module('mpdxApp')
                     return phone || '';
                 };
 
-                $scope.pledgeFrequencyStr = function(pledgeFrequency){
-                    switch(pledgeFrequency) {
-                        case '0.23076923076923':
-                            return 'Weekly';
-                        case '0.46153846153846':
-                            return 'Fortnightly';
-                        case '1':
-                            return 'Monthly';
-                        case '2':
-                            return 'Bi-Monthly';
-                        case '3':
-                            return 'Quarterly';
-                        case '4':
-                            return 'Quad-Monthly';
-                        case '6':
-                            return 'Semi-Annual';
-                        case '12':
-                            return 'Annual';
-                        case '24':
-                            return 'Biennial';
-                    }
-                };
-
                 $scope.hasSendNewsletterError = function() {
                     data = contactCache.getFromCache($scope.contact.id)
                     contact = data.contact
