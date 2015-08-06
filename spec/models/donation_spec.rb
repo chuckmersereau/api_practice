2559706b6d4ee6b5673a4e7c1606dd6590b70626
chencuicru <chen.cui@cru.org>
2015-08-06 16:00:50 -0400

    Add contact to appeal when donation added

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -8,4 +8,39 @@ describe Donation do
       expect(donation.localized_date).to eq 'April 05, 2015'
     end
   end
+
+  context 'add_appeal_contacts' do
+    let(:appeal) { create(:appeal) }
+
+    it 'adds a contact to the appeal if that contacts donation is added' do
+      contact = create(:contact, account_list: appeal.account_list)
+      contact.donor_accounts << donation.donor_account
+      donation.update(appeal: appeal, appeal_amount: 1)
+      expect(appeal.contacts.reload).to include(contact)
+    end
+
+    it "doesn't adds a contact to the appeal if that contact is in different account list" do
+      account_list = create(:account_list)
+      contact = create(:contact, account_list: account_list)
+      contact.donor_accounts << donation.donor_account
+      donation.update(appeal: appeal, appeal_amount: 1)
+      expect(appeal.contacts.reload).to_not include(contact)
+    end
+
+    it "doesn't adds a contact to the appeal if that contact is in different donor account" do
+      contact = create(:contact, account_list: appeal.account_list)
+      contact.donor_accounts << create(:donor_account)
+      donation.update(appeal: appeal, appeal_amount: 1)
+      expect(appeal.contacts.reload).to_not include(contact)
+    end
+
+    it "doesn't adds a contact to the appeal if that contact is in different donor account" do
+      contact = create(:contact, account_list: appeal.account_list)
+      donor_account = create(:donor_account)
+      contact.donor_accounts << donor_account
+      new_donation = create(:donation, appeal: appeal, appeal_amount: 1, donor_account_id: donor_account.id)
+      expect(new_donation.donor_account).to eq(contact.donor_accounts.first)
+      expect(appeal.contacts.reload).to include(contact)
+    end
+  end
 end
