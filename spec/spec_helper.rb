2da171a2bce3ffeadff9704282e31722a8dcd0f1
draffensperger <d.raffensperger@gmail.com>
2015-08-18 13:48:06 -0400

    Combine coverage results for js and unit specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,7 @@
-# uncomment the following line to use spork with the debugger
-# require 'spork/ext/ruby-debug'
-
-require 'coveralls'
-Coveralls.wear!('rails')
+if ENV['COVERALLS_REPO_TOKEN']
+  require 'coveralls'
+  Coveralls.wear_merged!('rails')
+end
 
 def start_simplecov
   require 'simplecov'
