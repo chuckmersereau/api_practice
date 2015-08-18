0740a5ac2506e67e611c9e6c5bc1517fb882b234
draffensperger <d.raffensperger@gmail.com>
2015-08-18 13:15:36 -0400

    Go back to running js specs separate, but run first

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -73,10 +73,8 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-  unless ENV['COVERALLS_REPO_TOKEN']
-    config.filter_run focus: true
-    config.filter_run_excluding js: true
-  end
+  config.filter_run focus: true
+  config.filter_run_excluding js: true
   config.run_all_when_everything_filtered = true
   config.include Devise::TestHelpers, type: :controller
   config.include FactoryGirl::Syntax::Methods
