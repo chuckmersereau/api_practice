e306b390196dbe9f2ffe4b9ebb520aa9fc00b870
draffensperger <d.raffensperger@gmail.com>
2015-08-25 12:41:49 -0400

    Fix referrals on hand calculation

--- spec/requests/api/v1/progress_controller_spec.rb
+++ spec/requests/api/v1/progress_controller_spec.rb
@@ -11,12 +11,24 @@ describe Api::V1::ProgressController do
 
     before do
       stub_auth
-      get '/api/v1/progress?access_token=' + user.access_token
     end
 
     it 'responds 200' do
+      get '/api/v1/progress?access_token=' + user.access_token
       expect(response).to be_success
       expect(JSON.parse(response.body)['contacts']['active']).to eq 1
+    end
+
+    it 'correctly counts the number of active referrals' do
+      referral1 = create(:contact, account_list: contact.account_list, status: 'Ask in Future')
+      referral2 = create(:contact, account_list: contact.account_list, status: 'Contact for Appointment')
+      contact.referrals_by_me << referral1
+      contact.referrals_by_me << referral2
+
+      get '/api/v1/progress?access_token=' + user.access_token
+      counts = assigns(:counts)
+      expect(counts[:contacts][:referrals]).to eq 2
+      expect(counts[:contacts][:referrals_on_hand]).to eq 2
     end
   end
 end
