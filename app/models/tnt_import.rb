532147e3509f7f6cee53ac9813d0881954d65fe1
draffensperger <d.raffensperger@gmail.com>
2015-09-15 12:55:44 -0400

    Sanitize commas in Tnt imported tags

--- app/models/tnt_import.rb
+++ app/models/tnt_import.rb
@@ -93,14 +93,18 @@ class TntImport
 
     Array.wrap(xml['GroupContact']['row']).each do |row|
       group = groups_by_id[row['GroupID']]
-      tags = [group[:description].gsub(' ', '-')]
-      tags << group[:category].gsub(' ', '-') if group[:category]
+      tags = [group_to_tag(group[:description])]
+      tags << group_to_tag(group[:category]) if group[:category]
 
       tags_list = @tags_by_contact_id[row['ContactID']]
       tags_list ||= []
       tags_list += tags
       @tags_by_contact_id[row['ContactID']] = tags_list
     end
+  end
+
+  def group_to_tag(group_name)
+    group_name.gsub(/\s|,/, '-')
   end
 
   def import_contact(row)
