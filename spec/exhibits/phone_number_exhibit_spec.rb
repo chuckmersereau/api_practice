d155a95cfe53e19a57270854360841de274a63e0
Lael Watkins <lael.watkins@cru.org>
2015-09-21 14:32:05 -0400

    Added additional specs for coverage

--- spec/exhibits/phone_number_exhibit_spec.rb
+++ spec/exhibits/phone_number_exhibit_spec.rb
@@ -15,8 +15,20 @@ describe PhoneNumberExhibit do
     expect(subject.number).to be_nil
   end
 
-  it 'should return an extension when included in the number' do
+  it 'should return an extension when included in the local number' do
     phone_number.number = '2135555555;ext=1234'
     expect(subject.number).to eq('(213) 555-5555 ext 1234')
+  end
+
+  it 'should return an e164 formatted number when an intl number is provided' do
+    phone_number.country_code = ''
+    phone_number.number = '613-555-55555'
+    expect(subject.number).to eq('+61355555555')
+  end
+
+  it 'should return an e164 formatted number and ext when included with an intl number' do
+    phone_number.country_code = ''
+    phone_number.number = '613-555-55555;ext=1234'
+    expect(subject.number).to eq('+61355555555 ext 1234')
   end
 end
