00248f214e846d5de9592f5bba451cc933759696
draffensperger <d.raffensperger@gmail.com>
2015-12-13 16:08:39 -0500

    Fix travis script to fail if tests fail

--- bin/travis_tests.sh
+++ bin/travis_tests.sh
@@ -2,18 +2,25 @@
 # Each build runs either rubocop or karma and then half of the specs
 # We also run two tests groups per worker since each worker gets 1.5 cores
 
+rubocop_or_karma() {
+  if [ "$TEST_SUITE" = "1" ]
+  then
+    bundle exec rubocop
+  else
+    npm install && bundle exec rake karma:run
+  fi
+}
 
-if [ $TEST_SUITE = 1 ]
-then
-  PHANTOMJS_CDNURL=https://bitbucket.org/ariya/phantomjs/downloads npm install
-  bundle exec rake karma:run
-  TEST_GROUPS=1,2
-else
-  bundle exec rubocop
-  TEST_GROUPS=3,4
-fi
+parallel_test() {
+  if [ "$TEST_SUITE" = "1" ]
+  then
+    TEST_GROUPS=1,2
+  else
+    TEST_GROUPS=3,4
+  fi
 
-DISABLE_SPRING=1 bundle exec parallel_test spec/ -n 4 \
-  --only-group $TEST_GROUPS --group-by filesize --type rspec
+  DISABLE_SPRING=1 bundle exec parallel_test spec/ -n 4 \
+    --only-group $TEST_GROUPS --group-by filesize --type rspec
+}
 
-bundle exec rake coveralls:push
+rubocop_or_karma && parallel_test && bundle exec rake coveralls:push
