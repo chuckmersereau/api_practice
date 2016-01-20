b579aba192f34d215596b13e6c3145274e4daa4e
draffensperger <d.raffensperger@gmail.com>
2016-01-20 09:31:20 -0500

    Use yield_control matcher for SidekiqWhodunnit spec

--- spec/lib/sidekiq_whodunnit_spec.rb
+++ spec/lib/sidekiq_whodunnit_spec.rb
@@ -6,13 +6,9 @@ describe SidekiqWhodunnit do
       'class' => 'Worker', 'enqueued_at' => 1,
       'args' => [1, :a, { c: 'hi' }], 'jid' => '1', 'x' => 2
     }
-
-    block_called = false
-    SidekiqWhodunnit.new.call(double, job, double) do
-      block_called = true
-    end
-    expect(block_called).to be true
-
+    expect do |block|
+      SidekiqWhodunnit.new.call(double, job, double, &block)
+    end.to yield_control
     expect(PaperTrail.whodunnit).to eq 'Worker [1, :a, {:c=>"hi"}]'
   end
 end
