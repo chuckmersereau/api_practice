38c5b010619d89009e1ec9c461035ee44ab3c730
adammeyer <adam.meyer@cru.org>
2016-02-17 14:42:35 -0500

    pledges total spec verbage update

--- spec/models/account_list/pledges_total_spec.rb
+++ spec/models/account_list/pledges_total_spec.rb
@@ -3,26 +3,26 @@ require 'spec_helper'
 describe AccountList::PledgesTotal do
   let(:account_list) { create(:account_list) }
 
-  it 'missing currency rate defaults to 1' do
+  it 'defaults unknown currency rates to 1' do
     rate = AccountList::PledgesTotal.new(account_list, account_list.contacts).latest_rate('ANY')
     expect(rate).to eq(1)
   end
 
-  it 'total pledges calculation with no Financial Partners' do
+  it 'calculates total pledges with no Financial Partners' do
     create(:contact, account_list: account_list, pledge_amount: 15, pledge_currency: 'USD', status: 'Partner - Special')
     create(:contact, account_list: account_list, pledge_amount: 10, pledge_currency: 'USD', status: '')
 
     expect(account_list.total_pledges).to eq(0)
   end
 
-  it 'total pledges calculation' do
+  it 'calculates total pledges' do
     create(:contact, account_list: account_list, pledge_amount: 15, pledge_currency: 'USD', status: 'Partner - Financial')
     create(:contact, account_list: account_list, pledge_amount: 10, pledge_currency: 'USD', status: '')
 
     expect(account_list.total_pledges).to eq(15)
   end
 
-  it 'total pledges currency conversion' do
+  it 'calculates total pledges with currency conversion' do
     create(:currency_rate, rate: 0.8, exchanged_on: '2016-02-15', code: 'EUR')
     create(:currency_rate, rate: 0.75, exchanged_on: '2016-01-15', code: 'EUR')
     create(:currency_rate, rate: 75.20, exchanged_on: '2016-02-15', code: 'RUB')
