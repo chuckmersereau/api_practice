b70a91516ecb27af98a5fe2447de60fe644c9d44
adammeyer <adam.meyer@cru.org>
2016-02-17 09:44:59 -0500

    latest_rate method

--- app/models/account_list/pledges_total.rb
+++ app/models/account_list/pledges_total.rb
@@ -5,20 +5,21 @@ class AccountList::PledgesTotal
 
   def default_currency_rate
     return 1 if @account_list.default_currency === 'USD'
-
-    rate = CurrencyRate.where(code: @account_list.default_currency).order(exchanged_on: :desc).limit(1).first
-    rate ? rate.rate : 1
+    latest_rate(@account_list.default_currency)
   end
 
   def monthly_pledge_usd(partner)
     return partner.monthly_pledge if partner.pledge_currency === 'USD'
-
-    rate = CurrencyRate.where(code: partner.pledge_currency).order(exchanged_on: :desc).limit(1).first
-    partner.monthly_pledge / rate.rate if rate
+    partner.monthly_pledge / latest_rate(partner.pledge_currency)
   end
 
   def total(partners)
     total_usd = partners.map(&method(:monthly_pledge_usd)).reduce(&:+)
     total_usd / default_currency_rate
+  end
+
+  def latest_rate(currency)
+    rate = CurrencyRate.where(code: currency).order(exchanged_on: :desc).limit(1).first
+    rate ? rate.rate : 1
   end
 end
