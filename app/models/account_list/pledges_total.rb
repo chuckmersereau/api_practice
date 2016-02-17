e0a29d629bd2c2693e33f0955731ac931efc7f35
adammeyer <adam.meyer@cru.org>
2016-02-17 11:18:47 -0500

    use sum method

--- app/models/account_list/pledges_total.rb
+++ app/models/account_list/pledges_total.rb
@@ -8,17 +8,16 @@ class AccountList::PledgesTotal
   end
 
   def monthly_pledge_usd(partner)
-    return partner.monthly_pledge if partner.pledge_currency === 'USD'
     partner.monthly_pledge / latest_rate(partner.pledge_currency)
   end
 
   def total
-    total_usd = @account_list.contacts.financial_partners.map(&method(:monthly_pledge_usd)).reduce(&:+)
+    total_usd = @account_list.contacts.financial_partners.map(&method(:monthly_pledge_usd)).sum
     total_usd / default_currency_rate
   end
 
   def latest_rate(currency)
-    return 1 if currency === 'USD'
+    return 1 if currency == 'USD'
 
     rate = CurrencyRate.where(code: currency).order(exchanged_on: :desc).limit(1).first
     rate ? rate.rate : 1
