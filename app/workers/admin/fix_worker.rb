d111edfc177086060ca245edb2e55366d8f15d5a
draffensperger <d.raffensperger@gmail.com>
2016-02-23 13:32:00 -0500

    Start on generic fix worker

--- app/workers/admin/fix_worker.rb
+++ app/workers/admin/fix_worker.rb
@@ -1,9 +1,9 @@
-class Admin::PrimaryAddressFixWorker
+class Admin::FixWorker
   include Sidekiq::Worker
 
   sidekiq_options unique: true, retry: false
 
-  def perform(account_list_id)
+  def perform(fix_class, account_list_id)
     AccountList.find(account_list_id).contacts.find_each do |contact|
       Admin::PrimaryAddressFix.new(contact).fix!
     end
--- spec/workers/admin/fix_worker_spec.rb
+++ spec/workers/admin/fix_worker_spec.rb
@@ -1,14 +1,11 @@
 require 'spec_helper'
 
-describe Admin::PrimaryAddressFixWorker, '#perform' do
-  it 'finds account list and runs primary address fix for each contact in it' do
-    contact = double('contact')
-    contacts = double('contacts')
-    allow(contacts).to receive(:find_each).and_yield(contact)
+describe Admin::FixWorker, '#perform' do
+  it 'finds account list and runs the specified fix for it' do
     account_list = double('account_list', contacts: contacts)
     allow(AccountList).to receive(:find).with(1) { account_list }
     worker = Admin::PrimaryAddressFixWorker.new
-    primary_address_fix = double('primary_address_fix', fix!: nil)
+
     allow(Admin::PrimaryAddressFix).to receive(:new)
       .with(contact) { primary_address_fix }
 
