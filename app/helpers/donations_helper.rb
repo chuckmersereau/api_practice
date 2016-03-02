b0144399f5fd74b70c621a934fa9272b3cea910e
adammeyer <adam.meyer@cru.org>
2016-03-02 16:25:49 -0500

    DonationsHelper

---
+++ app/helpers/donations_helper.rb
@@ -0,0 +1,6 @@
+module DonationsHelper
+  def totals_by_currency(donations)
+    donations.group_by{ |d| d.currency == '' ? current_account_list.default_currency : d.currency }
+        .map{ |currency, amount| {:currency => currency, :count => amount.sum {1}, :sum => amount.sum {|j| j.amount.to_f} }}
+  end
+end
