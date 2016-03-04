3b056fff44472920fe3e728856e5fbd53c4613e7
adammeyer <adam.meyer@cru.org>
2016-03-04 14:30:59 -0500

    formatting fixes

--- app/helpers/donations_helper.rb
+++ app/helpers/donations_helper.rb
@@ -1,6 +1,12 @@
 module DonationsHelper
   def totals_by_currency(donations)
-    donations.group_by{ |d| d.currency == '' ? current_account_list.default_currency : d.currency }
-        .map{ |currency, amount| {:currency => currency, :count => amount.sum {1}, :sum => amount.sum {|j| j.amount.to_f} }}
+    donations.group_by { |d| d.currency == '' ? current_account_list.default_currency : d.currency }
+      .map do |currency, amount|
+      {
+        currency: currency,
+        count: amount.sum { 1 },
+        sum: amount.sum { |j| j.amount.to_f }
+      }
+    end
   end
 end
