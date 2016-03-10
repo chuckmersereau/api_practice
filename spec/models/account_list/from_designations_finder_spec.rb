e0616bedc889e91f09b6c2fc820efbec988a04f0
draffensperger <d.raffensperger@gmail.com>
2016-03-10 09:53:55 -0500

    Prevent users from seeing higher-level org accounts

--- spec/models/account_list/from_designations_finder_spec.rb
+++ spec/models/account_list/from_designations_finder_spec.rb
@@ -7,16 +7,28 @@ describe AccountList::FromDesignationsFinder, '#account_list' do
     expect(finder.account_list).to be_nil
   end
 
-  it 'returns the account list that contains the specified designations' do
+  it 'returns the account list whose designations for that organization match' do
+    org1 = create(:organization)
+    org2 = create(:organization)
+    da1 = create(:designation_account, designation_number: '1', organization: org1)
+    da2 = create(:designation_account, designation_number: '2', organization: org1)
+    da3 = create(:designation_account, designation_number: '3', organization: org2)
+    account_list = create(:account_list)
+    account_list.designation_accounts << [da1, da2, da3]
+    finder = AccountList::FromDesignationsFinder.new(%w(1 2), org1.id)
+
+    expect(finder.account_list).to eq account_list
+  end
+
+  it 'returns nil if no account list whose designations for that org match' do
     org = create(:organization)
     da1 = create(:designation_account, designation_number: '1', organization: org)
     da2 = create(:designation_account, designation_number: '2', organization: org)
-    da3 = create(:designation_account, designation_number: '3', organization: org)
     account_list = create(:account_list)
-    account_list.designation_accounts << [da1, da2, da3]
-    finder = AccountList::FromDesignationsFinder.new(%w(1 2), org.id)
+    account_list.designation_accounts << [da1, da2]
+    finder = AccountList::FromDesignationsFinder.new(%w(1), org.id)
 
-    expect(finder.account_list).to eq account_list
+    expect(finder.account_list).to be_nil
   end
 
   it 'returns nil if no account list contains all the specified designations' do
