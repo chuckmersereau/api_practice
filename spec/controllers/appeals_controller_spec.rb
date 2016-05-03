a3643b27e7bfcd18aae78b7d85fbe16b761033ed
Spencer Oberstadt <soberstadt@gmail.com>
2016-05-03 09:57:45 -0400

    fix appeals controller spec

--- spec/controllers/appeals_controller_spec.rb
+++ spec/controllers/appeals_controller_spec.rb
@@ -1,12 +1,16 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe AppealsController, type: :controller do
+  describe 'GET #show' do
+    before(:each) do
+      @user = FactoryGirl.create(:user_with_account)
+      sign_in(:user, @user)
+    end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
+    it 'returns http success' do
+      appeal = create(:appeal, account_list: @user.account_lists.first)
+      get :show, id: appeal.id
       expect(response).to have_http_status(:success)
     end
   end
-
 end
