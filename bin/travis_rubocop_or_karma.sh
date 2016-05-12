b7ea3aeabdb3f60c0584ecd08062028923b0a73d
Dave Raffensperger <d.raffensperger@gmail.com>
2016-05-12 08:13:06 -0400

    Add ES Lint and ignore current violations (#869)
    * Add ES Lint and ignore current violations
    
    * Add back npm packages
    
    * Add recent js files to eslintignore

--- bin/travis_rubocop_or_karma.sh
+++ bin/travis_rubocop_or_karma.sh
@@ -2,5 +2,5 @@ if [ "$TEST_SUITE" = "1" ]
 then
   bundle exec rake karma:run && bundle exec rspec spec/ --tag js
 else
-  bundle exec rubocop
+  bundle exec rubocop && ./node_modules/.bin/eslint app/assets/javascripts
 fi
