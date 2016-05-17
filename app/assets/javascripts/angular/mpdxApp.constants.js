5e2d8bcc50df7648ee40f6127a7466fc4af85416
Dave Raffensperger <d.raffensperger@gmail.com>
2016-05-17 12:34:47 -0400

    Cleanup appealsList js code a bit (#881)

--- app/assets/javascripts/angular/mpdxApp.constants.js
+++ app/assets/javascripts/angular/mpdxApp.constants.js
@@ -2,5 +2,7 @@
     angular
         .module('mpdxApp')
         .constant('moment', window.moment)
-        .constant('_', window._);
+        .constant('_', window._)
+        .constant('__', window.__)
+        .constant('$', window.$);
 })();
