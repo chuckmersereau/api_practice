4ae0c8b9bfec1fc8297b98f01938ba66ffea8618
Scotty Waggoner <ozzieorca@gmail.com>
2016-06-20 14:03:24 -0700

    Add karma-coverage
    - also added karma-mocha-reporter for better test result formatting

--- spec/karma/config/unit.js
+++ spec/karma/config/unit.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
 
         // test results reporter to use
         // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters: ['progress'],
+        reporters: ['progress', 'mocha', 'coverage'],
 
 
         // web server port
@@ -64,7 +64,12 @@ module.exports = function(config) {
 
         // Preprocessors
         preprocessors: {
-            '/**/*.coffee':'coffee'
+            '/**/*.coffee':'coffee',
+            './app/assets/javascripts/**/*!(.spec).js': ['coverage']
+        },
+
+        coverageReporter: {
+            type : 'lcov'
         }
 
     });
