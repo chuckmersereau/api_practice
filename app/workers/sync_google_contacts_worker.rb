4be7812c295ac09af22765b3192cbb6fedbb1e8a
Pablo Marti <startpablo@gmail.com>
2016-06-24 13:04:48 -0600

    Refactor for sync the account lists during a period of 24h

--- app/workers/sync_google_contacts_worker.rb
+++ app/workers/sync_google_contacts_worker.rb
@@ -1,10 +1,13 @@
+require 'async'
+
 class SyncGoogleContactsWorker
+  include Async
   include Sidekiq::Worker
   sidekiq_options unique: true
 
   def perform
-    AccountList.joins(:google_integrations)
-         .where(google_integrations: { contacts_integration: true })
-         .find_each(&:queue_sync_with_google_contacts)
+    account_lists = AccountList.joins(:google_integrations)
+                        .where(google_integrations: { contacts_integration: true })
+    AsyncScheduler.schedule_over_24h(account_lists, :queue_sync_with_google_contacts)
   end
 end
--- spec/workers/sync_google_contacts_worker_spec.rb
+++ spec/workers/sync_google_contacts_worker_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe SyncGoogleContactsWorker do
+describe SyncGoogleContactsWorker, sidekiq: :testing_disabled do
   context 'Sync user\'s Google contacts' do
     let!(:user) { create(:user_with_account) }
     let!(:google_integration) do
@@ -9,12 +9,14 @@ describe SyncGoogleContactsWorker do
              contacts_integration: true,
              account_list: user.account_lists.first)
     end
-
     it 'queues the sync with Google contacts for the account list' do
+      clear_uniqueness_locks
       expect do
         subject.perform
-      end.to change(LowerRetryWorker.jobs, :count).by(1)
-      expect(LowerRetryWorker.jobs.last['args']).to eq google_integration.account_list.queue_sync_with_google_contacts
+      end.to change(Sidekiq::ScheduledSet.new, :size).by(1)
+      job = Sidekiq::ScheduledSet.new.to_a.first
+      expect(job['class']).to eq 'AccountList'
+      expect(job['args']).to contain_exactly(1, 'queue_sync_with_google_contacts')
     end
   end
 end
