f60e5702b5974bec7a6f72d2c8efd242998513f9
Kewei Jiang <kewei.jiang@cru.org>
2016-06-24 12:23:51 +1200

    One more test for activity type filter

--- spec/models/task_filter_spec.rb
+++ spec/models/task_filter_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 RSpec.describe TaskFilter, type: :model do
   before do
-    create(:task, completed: true, starred: false, no_date: false)
-    create(:task, completed: false, starred: true, no_date: false)
-    create(:task, completed: false, starred: false, no_date: true)
+    create(:task, completed: true, starred: false, no_date: false, activity_type: 'Email')
+    create(:task, completed: false, starred: true, no_date: false, activity_type: 'Call')
+    create(:task, completed: false, starred: false, no_date: true, activity_type: 'Call')
   end
 
   it 'filters through completed tasks' do
@@ -27,6 +27,12 @@ RSpec.describe TaskFilter, type: :model do
 
   it 'filters through no_date tasks' do
     task_filter = TaskFilter.new(:no_date => true)
+    filtered = task_filter.filter(Task.all)
+    expect(filtered.length).to eq(1)
+  end
+
+  it 'filters through tasks by activity_type' do
+    task_filter = TaskFilter.new(:activity_type => 'Email')
     filtered = task_filter.filter(Task.all)
     expect(filtered.length).to eq(1)
   end
