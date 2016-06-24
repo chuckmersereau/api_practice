17b7764d4b780c2bce1761336037aaeac501e266
Pablo Marti <startpablo@gmail.com>
2016-06-24 09:11:26 -0600

    Fix spec

--- spec/workers/sync_google_contacts_worker_spec.rb
+++ spec/workers/sync_google_contacts_worker_spec.rb
@@ -2,24 +2,19 @@ require 'spec_helper'
 
 describe SyncGoogleContactsWorker do
   context 'Sync user\'s Google contacts' do
-    let(:user) { create(:user_with_account) }
-    let(:google_integration) do
+    let!(:user) { create(:user_with_account) }
+    let!(:google_integration) do
       create(:google_integration,
              calendar_integration: false,
              contacts_integration: true,
              account_list: user.account_lists.first)
     end
 
-    it 'runs sucessfully the worker ' do
+    it 'queues the sync with Google contacts for the account list' do
       expect do
         subject.perform
-      end.to_not raise_error
-    end
-
-    it 'calls the specified method on the given class' do
-      subject.perform do
-        expect(google_integration.account_list).to receive(:queue_sync_with_google_contacts)
-      end
+      end.to change(LowerRetryWorker.jobs, :count).by(1)
+      expect(LowerRetryWorker.jobs.last['args']).not_to eq google_integration.account_list.queue_sync_with_google_contacts
     end
   end
 end
