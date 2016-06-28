403700153441d21d12031f49f3898b26b75093ba
Kewei Jiang <kewei.jiang@cru.org>
2016-06-28 17:21:00 +1200

    More specs on contacts list

--- spec/features/contact_feature_spec.rb
+++ spec/features/contact_feature_spec.rb
@@ -11,13 +11,25 @@ describe 'create new contact', js: true do
   before :each do
     user = create(:user_with_account)
     login(user)
-    create(:contact, account_list: user.account_lists.first)
-    create(:contact, account_list: user.account_lists.first)
-    create(:contact, account_list: user.account_lists.first)
+    create(:contact, account_list: user.account_lists.first, name: 'Apostle John')
+    create(:contact, account_list: user.account_lists.first, name: 'Apostle Paul')
+    create(:contact, account_list: user.account_lists.first, name: 'Simon Peter')
+    create_list(:contact, 50, account_list: user.account_lists.first)
   end
 
   it 'displays a list of contacts' do
     visit '/contacts'
+    expect(find('.pagination')).to have_content '2'
+    expect(all('contact').length).to eq 25
+    click_on('Next')
+    expect(all('contact').length).to eq 25
+    click_on('Next')
     expect(all('contact').length).to eq 3
+  end
+
+  it 'correctly filters contacts through search' do
+    visit '/contacts'
+    fill_in('contact_name', with: 'Apostle')
+    expect(all('contact').length).to eq 2
   end
 end
