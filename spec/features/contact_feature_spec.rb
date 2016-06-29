4db622d0a0094c64529e272a802128ed947d89ce
Kewei Jiang <kewei.jiang@cru.org>
2016-06-29 16:46:43 +1200

    Test for merging

--- spec/features/contact_feature_spec.rb
+++ spec/features/contact_feature_spec.rb
@@ -19,8 +19,12 @@ describe 'create new contact', js: true do
 
   it 'displays a list of contacts' do
     visit '/contacts'
-    expect(find('.pagination')).to have_content '2'
     expect(all('contact').length).to eq 25
+  end
+
+  it 'pagination works' do
+    visit '/contacts'
+    expect(find('.pagination')).to have_content '2'
     click_on('Next')
     expect(all('contact').length).to eq 25
     click_on('Next')
@@ -31,5 +35,24 @@ describe 'create new contact', js: true do
     visit '/contacts'
     fill_in('contact_name', with: 'Apostle')
     expect(all('contact').length).to eq 2
+  end
+
+  it 'correctly merges 2 accounts together' do
+    visit '/contacts'
+    contact_checkboxes = all('contact input')
+    contact_checkboxes[0].set(true)
+    contact_checkboxes[1].set(true)
+    click_on('Merge')
+    contact_names = all('contact .name a')
+    contact_name_1 = contact_names[0].base.inner_html
+    contact_name_2 = contact_names[1].base.inner_html
+    merged_names = all('#merge_contact_names li')
+    expect(merged_names[0]).to have_content contact_name_1
+    expect(merged_names[1]).to have_content contact_name_2
+    expect(find('#merge_modal .warning-text')).to have_content 'This action'
+    click_button('Merge')
+    contact_people = all('contact .people a')
+    contact_people[0].to have_content contact_name_1
+    contact_people[1].to have_content contact_name_2
   end
 end
