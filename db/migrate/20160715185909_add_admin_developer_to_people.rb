f39d3db0ee3fae6445691f35ca65068304ddd10a
Pablo Martí <pablo.marti89@gmail.com>
2016-07-15 16:29:25 -0600

    Revert "Fix admin preferences"

--- db/migrate/20160715185909_add_admin_developer_to_people.rb
+++
@@ -1,6 +0,0 @@
-class AddAdminDeveloperToPeople < ActiveRecord::Migration
-  def change
-    add_column :people, :admin, :boolean, :default => false
-    add_column :people, :developer, :boolean, :default => false
-  end
-end
