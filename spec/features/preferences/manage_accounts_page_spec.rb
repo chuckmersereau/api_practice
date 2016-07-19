5a04667ef12698f96801f667884a43ba9344bb32
Kewei Jiang <kewei.jiang@cru.org>
2016-07-19 16:10:26 +1200

    Last of specs for invites and merges

--- spec/features/preferences/manage_accounts_page_spec.rb
+++ spec/features/preferences/manage_accounts_page_spec.rb
@@ -32,9 +32,55 @@ describe 'personal accounts preferences', js: true do
     expect(all('.account-users li').length).to eq 1
     account_list.reload
     expect(account_list.users.length).to eq 1
-    # all('.network_connections')[0].find('.delete').click
-    # sleep 3
-    # user_key_accounts.reload
-    # expect(user_key_accounts.length).to be 1
+  end
+
+  it 'Accounts merges correctly' do
+    expect(user.account_lists.length).to be 2
+
+    login_and_visit
+
+    current_panel = all('.panel-group .panel')[1]
+    current_panel.click
+    sleep 0.5
+    current_panel.find('.chosen-container').click
+    expect(current_panel.all('.chosen-results li').length).to eq 1
+    current_panel.all('.chosen-results li')[0].click
+    click_button('Merge')
+    sleep 1
+    expect(page).to have_content 'You only have access to this account'
+    expect(current_panel.all('.form-group').length).to be 0
+    user.reload
+    expect(user.account_lists.length).to be 1
+  end
+
+  it 'Accounts merges correctly in spouse tab' do
+    expect(user.account_lists.length).to be 2
+
+    login_and_visit
+
+    current_panel = all('.panel-group .panel')[2]
+    current_panel.click
+    sleep 0.5
+    current_panel.find('.chosen-container').click
+    expect(all('.panel-group .panel')[2].all('.chosen-results li').length).to eq 1
+    current_panel.all('.chosen-results li')[0].click
+    click_button('Merge')
+    sleep 1
+    expect(page).to have_content 'You only have access to this account'
+    expect(current_panel.all('.chosen-container').length).to be 0
+    user.reload
+    expect(user.account_lists.length).to be 1
+  end
+
+  it 'Invite e-mail correctly sends in spouse tab' do
+    login_and_visit
+
+    current_panel = all('.panel-group .panel')[2]
+    current_panel.click
+    sleep 0.5
+    fill_in('email', with: 'foo@bar.com')
+    expect(AccountListInvite).to receive(:send_invite).with(user, user.account_lists.find_by(name: 'Charles Spurgeon'), 'foo@bar.com')
+    click_button('Share Account')
+    sleep 1
   end
 end
