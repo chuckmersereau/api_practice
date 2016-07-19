1a6083b8ab47f4969d333148d80478628283443e
Pablo Marti <startpablo@gmail.com>
2016-07-19 16:03:54 -0600

    Refactor offlineOrg class and set appropiate the account list

--- app/models/offline_org.rb
+++ app/models/offline_org.rb
@@ -6,9 +6,9 @@ class OfflineOrg < DataServer
   end
 
   def import_profiles
-    designation_profile = create_designation_profile(account_list)
-    designation_account = create_designation_account
-    create_designation_profile_account(designation_profile, designation_account)
+    profile = create_designation_profile
+    create_designation_account(profile)
+    AccountList::FromProfileLinker.new(profile, @org_account).link_account_list!
   end
 
   def self.requires_username_and_password?
@@ -17,34 +17,20 @@ class OfflineOrg < DataServer
 
   private
 
-  def account_list
-    @org_account.user.account_lists.find_by(creator_id: @org_account.user.id)
+  def create_designation_profile
+    @org.designation_profiles.where(
+        user_id: @org_account.person_id,
+        name: @org_account.user.to_s,
+        code: @org_account.id.to_s
+    ).first_or_create
   end
 
-  def create_designation_profile(account_list)
-    designation_profile = DesignationProfile.find_or_create_by!(
-      user_id: @org_account.person_id,
-      organization_id: @org.id,
-      account_list_id: account_list.id
-    ) do |dp|
-      dp.code = @org_account.id.to_s
-    end
-    designation_profile.update(name: @org_account.user.to_s)
-    designation_profile
-  end
-
-  def create_designation_account
-    designation_account = DesignationAccount.find_or_create_by!(
-      organization_id: @org.id,
-      active: true,
-      designation_number: @org_account.id.to_s)
-    designation_account.update(name: @org_account.user.to_s)
-    designation_account
-  end
-
-  def create_designation_profile_account(designation_profile, designation_account)
-    DesignationProfileAccount.find_or_create_by!(
-      designation_account_id: designation_account.id,
-      designation_profile_id: designation_profile.id)
+  def create_designation_account(profile)
+    da = @org.designation_accounts.where(
+        designation_number: @org_account.id.to_s,
+        active: true
+    ).first_or_create
+    da.update(name: @org_account.user.to_s)
+    profile.designation_accounts << da unless profile.designation_accounts.include?(da)
   end
 end
