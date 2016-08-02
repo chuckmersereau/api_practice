f9cbfbc4b6ec000241668f904ad4ca94f326c6e5
Kewei Jiang <kewei.jiang@cru.org>
2016-08-02 18:48:56 +1200

    Another hacky fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -79,9 +79,23 @@ RSpec.configure do |config|
   config.before(:each, js: true) do
     page.driver.browser.url_blacklist = ["http://use.typekit.net"]
     Capybara.current_driver = Capybara.javascript_driver
-  end
 
-  config.assets.compile = false
+    config.before :all do
+      ENV['PRECOMPILE_ASSETS'] ||= begin
+        case self.class.metadata[:type]
+        when :feature, :view
+          STDOUT.write "Precompiling assets..."
+
+          require 'rake'
+          Rails.application.load_tasks
+          Rake::Task['assets:precompile'].invoke
+
+          STDOUT.puts " done."
+          Time.now.to_s
+        end
+      end
+    end
+  end
 
   # ## Mock Framework
   #
