7b574452cd4d12b225bcd73f118f0228440cd551
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-08-02 14:46:04 +1200

    force specs to run in RAILS_ENV=test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ def start_simplecov
   end
 end
 
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'webmock/rspec'
