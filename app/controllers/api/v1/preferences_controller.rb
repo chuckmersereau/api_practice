ac5af60b7d83241bd0512d0a17dad24ab96a5ee6
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-09-09 17:10:29 +1200

    HOTFIX: Notification preferences not saving

--- app/controllers/api/v1/preferences_controller.rb
+++ app/controllers/api/v1/preferences_controller.rb
@@ -23,8 +23,12 @@ class Api::V1::PreferencesController < Api::V1::BaseController
   def preference_params
     return {} unless params[:preference]
     preference_params = params[:preference]
+    notification_params = []
+    NotificationType.all.each do |type|
+      notification_params.push(type.class.to_s.split('::').last.to_s.underscore.to_sym => { actions: [] })
+    end
     preference_params.permit(:first_name, :last_name, :email, :time_zone, :locale, :monthly_goal, :default_account_list,
                              :tester, :home_country, :ministry_country, :currency, :salary_organization_id,
-                             :account_list_name)
+                             :account_list_name, *notification_params)
   end
 end
