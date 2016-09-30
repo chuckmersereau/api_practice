7a354942c293f3dabba67cbebd16d68d0291b5e6
Pablo Marti <startpablo@gmail.com>
2016-09-29 22:53:21 -0600

    Rollback test

--- app/workers/fix_donations_worker.rb
+++ app/workers/fix_donations_worker.rb
@@ -1,7 +1,7 @@
 require './dev/migrate/2016_07_14_fix_donations_without_designation_acc.rb'
 class FixDonationsWorker
   include Sidekiq::Worker
-  
+
   def perform(offset = 0)
     AddAccountsToDonations.new.add_accounts_to_donations(offset)
   end
