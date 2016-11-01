f055506ca6c983826580bea253110f91b2264667
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-11-01 22:00:01 +1300

    try three parallel tests

--- bin/travis_parallel_test.sh
+++ bin/travis_parallel_test.sh
@@ -1,9 +1,9 @@
 if [ "$TEST_SUITE" = "1" ]
 then
-  TEST_GROUPS=1,2
+  TEST_GROUPS=1,2,3
 else
-  TEST_GROUPS=3,4
+  TEST_GROUPS=4,5,6
 fi
 
-DISABLE_SPRING=1 bundle exec parallel_test spec/ -n 4 \
+DISABLE_SPRING=1 bundle exec parallel_test spec/ -n 6 \
   --only-group $TEST_GROUPS --group-by filesize --type rspec --verbose
