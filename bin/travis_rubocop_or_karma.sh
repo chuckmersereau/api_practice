6e7a71643d2abd0cd75b570d83daebc8cf1afa3a
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-11-01 15:33:30 +1300

    drop js and older views

--- bin/travis_rubocop_or_karma.sh
+++
@@ -1,6 +0,0 @@
-if [ "$TEST_SUITE" = "1" ]
-then
-  bundle exec rake karma:run && bundle exec rspec spec/ --tag js
-else
-  bundle exec rubocop && ./node_modules/.bin/eslint app/assets/javascripts
-fi
