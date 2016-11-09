1478fa67506ba1ff6f2e7827decab5fcb6f82b1f
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-11-10 12:41:38 +1300

    remove new_relic for now

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -1,7 +1,6 @@
 class MonitorsController < ActionController::API
   skip_before_action :ensure_login
   skip_before_action :ensure_setup_finished
-  newrelic_ignore
 
   def lb
     ActiveRecord::Base.connection.select_values('select id from people limit 1')
