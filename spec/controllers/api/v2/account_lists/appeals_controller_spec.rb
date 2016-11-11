46fe4d21719a2aedaef32c31bc595b73ba07e1bd
Pablo Marti <startpablo@gmail.com>
2016-11-11 09:25:47 -0600

    Remove comments

--- spec/controllers/api/v2/account_lists/appeals_controller_spec.rb
+++ spec/controllers/api/v2/account_lists/appeals_controller_spec.rb
@@ -5,10 +5,6 @@ describe Api::V2::AccountLists::AppealsController, type: :controller do
   let!(:user) { create(:user_with_full_account) }
   let!(:account_list) { user.account_lists.first }
   let(:account_list_id) { account_list.id }
-  # let!(:contact) { create(:contact, account_list_id: account_list.id) }
-  # let!(:donor_account) { create(:donor_account) }
-  # let!(:designation_account) { create(:designation_account) }
-  # let!(:donations) { create_list(:donation, 2, donor_account: donor_account, designation_account: designation_account, amount: 10.00) }
   let!(:appeal) { create(:appeal, account_list: account_list) }
   let(:id) { appeal.id }
 
