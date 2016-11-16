4bd1d133e2b69f7f0f246a49754beb84894c18da
Pablo Marti <startpablo@gmail.com>
2016-11-16 16:13:56 -0600

    Fix appeals spec

--- spec/controllers/api/v2/appeals_controller_spec.rb
+++ spec/controllers/api/v2/appeals_controller_spec.rb
@@ -5,13 +5,14 @@ describe Api::V2::AppealsController, type: :controller do
   let!(:user) { create(:user_with_full_account) }
   let!(:account_list) { user.account_lists.first }
   let(:account_list_id) { account_list.id }
-  let!(:appeal) { create(:appeal, account_list: account_list) }
+  let!(:appeal) { create(:appeal, account_list_id: account_list_id) }
   let(:id) { appeal.id }
 
   let(:resource) { appeal }
-  let(:parent_path) { { account_list_id: account_list_id } }
-  let(:correct_attributes) { attributes_for(:appeal, name: 'Appeal 2') }
-  let(:incorrect_attributes) { { account_list_id: nil } }
+  let(:parent_param) { { account_list_id: account_list_id } }
+  let(:correct_attributes) { attributes_for(:appeal, name: 'Appeal 2', account_list_id: account_list_id) }
+  let(:unpermitted_attributes) { attributes_for(:appeal, name: 'Appeal 3', account_list_id: create(:account_list).id) }
+  let(:incorrect_attributes) { attributes_for(:appeal, account_list_id: nil) }
 
   include_examples 'index_examples'
 
