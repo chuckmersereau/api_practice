e677624fee5cf81b1ffe835b08d78059eabc6ada
Pablo Marti <startpablo@gmail.com>
2016-11-17 08:46:02 -0600

    Refactor spec

--- spec/controllers/api/v2/appeals_controller_spec.rb
+++ spec/controllers/api/v2/appeals_controller_spec.rb
@@ -1,24 +1,22 @@
 require 'spec_helper'
+require 'support/shared_controller_examples'
 
-describe Api::V2::AppealsController, type: :controller do
-  let(:resource_type) { 'appeal' }
-  let!(:user) { create(:user_with_full_account) }
-  let!(:account_list) { user.account_lists.first }
-  let(:account_list_id) { account_list.id }
-  let!(:appeal) { create(:appeal, account_list_id: account_list_id) }
-  let(:id) { appeal.id }
-
-  let(:resource) { appeal }
-  let(:parent_param) { { account_list_id: account_list_id } }
-  let(:correct_attributes) { attributes_for(:appeal, name: 'Appeal 2', account_list_id: account_list_id) }
+RSpec.describe Api::V2::AppealsController, type: :controller do
+  let(:user) { create(:user_with_account) }
+  let(:account_list) { user.account_lists.first }
+  let(:resource_type) { :appeal }
+  let!(:resource) { create(:appeal, account_list: account_list) }
+  let(:id) { resource.id }
+  let(:parent_param) { { account_list_id: account_list.id } }
+  let(:correct_attributes) { attributes_for(:appeal, name: 'Appeal 2', account_list_id: account_list.id) }
   let(:unpermitted_attributes) { attributes_for(:appeal, name: 'Appeal 3', account_list_id: create(:account_list).id) }
   let(:incorrect_attributes) { attributes_for(:appeal, account_list_id: nil) }
-
-  include_examples 'index_examples'
-
+  
   include_examples 'show_examples'
 
   include_examples 'update_examples'
 
   include_examples 'destroy_examples'
+
+  include_examples 'index_examples'
 end
