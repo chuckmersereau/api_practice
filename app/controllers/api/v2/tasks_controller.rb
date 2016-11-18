b09b4527f6e19da2c7fe8e88778c245821bf85be
Pablo Marti <startpablo@gmail.com>
2016-11-18 12:57:09 -0600

    Restore Task

--- app/controllers/api/v2/tasks_controller.rb
+++ app/controllers/api/v2/tasks_controller.rb
@@ -6,14 +6,6 @@ class Api::V2::TasksController < Api::V2::ResourceController
   end
 
   def resource_scope
-    task_scope
-  end
-
-  def task_scope
-    Task.where(filter_params)
-  end
-
-  def permited_filters
-    [:account_list_id]
+    Task.that_belong_to(current_user)
   end
 end
