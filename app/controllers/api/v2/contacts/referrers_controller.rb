d2c4d0c6bd34a7afe3b58b56dfdf7435db2ca7ea
Will Cosgrove <will@willcosgrove.com>
2016-12-08 14:02:03 -0600

    Add filter scoping to contact referrers endpoint

--- app/controllers/api/v2/contacts/referrers_controller.rb
+++ app/controllers/api/v2/contacts/referrers_controller.rb
@@ -20,7 +20,8 @@ class Api::V2::Contacts::ReferrersController < Api::V2Controller
   end
 
   def load_referrers
-    @referrers = referrer_scope.reorder(sorting_param)
+    @referrers = referrer_scope.where(filter_params)
+                               .reorder(sorting_param)
                                .page(page_number_param)
                                .per(per_page_param)
   end
