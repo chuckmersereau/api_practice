a4e12be6317d547f6c4902682c0ec47be404c667
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-12-16 15:00:38 +1300

    remove uuid for versions table

--- db/migrate/20161216004227_add_uuid_to_versions.rb
+++
@@ -1,6 +0,0 @@
-class AddUuidToVersions < ActiveRecord::Migration
-  def change
-    add_column :versions, :uuid, :uuid, null: false, default: 'uuid_generate_v4()'
-    add_index :versions, :uuid, unique: true
-  end
-end
