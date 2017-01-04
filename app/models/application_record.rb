30ad2942f8d856118e354e59e88a4b2abf3f0225
Sheldon <sheldond@users.noreply.github.com>
2017-01-04 11:28:24 -0600

    Fix comparison of updated_at_was and updated_in_db_at (#81)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,11 +3,15 @@ require 'securerandom'
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  attr_accessor :updated_in_db_at
+  attr_reader :updated_in_db_at
 
   before_save :generate_uuid, on: :create
   validate :presence_of_updated_in_db_at, on: :update_from_controller
   validate :value_of_updated_in_db_at, on: :update_from_controller
+
+  def updated_in_db_at=(value)
+    @updated_in_db_at = value.is_a?(Time) ? value : Time.parse(value.to_s)
+  end
 
   private
 
@@ -22,7 +26,7 @@ class ApplicationRecord < ActiveRecord::Base
   end
 
   def value_of_updated_in_db_at
-    return if updated_at_was.to_s == updated_in_db_at.to_s
+    return if updated_at_was.to_i == updated_in_db_at.to_i
     errors.add(:updated_in_db_at, 'is not equal to the current value in the database')
   end
 end
