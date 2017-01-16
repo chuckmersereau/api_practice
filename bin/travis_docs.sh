a5c95fa0c2e106d8602ed5972dbdcff126e92422
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-01-16 12:56:12 -0500

    add staging docs credentials

--- bin/travis_docs.sh
+++ bin/travis_docs.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in secrets.tar.enc -out secrets.tar -d
+tar xvf secrets.tar
+
 if [ "$TRAVIS_BRANCH" != "staging" ]; then
   SOURCE_BRANCH="staging"
   REPO="https://github.com/CruGlobal/mpdx_staging_docs.git"
+  chmod 600 deploy_staging_key
+  eval `ssh-agent -s`
+  ssh-add deploy_staging_key
 elif [ "$TRAVIS_BRANCH" != "master" ]; then
   SOURCE_BRANCH="master"
   REPO="https://github.com/CruGlobal/mpdx_docs.git"
+  chmod 600 deploy_key
+  eval `ssh-agent -s`
+  ssh-add deploy_key
 fi
 
 function doCompile {
@@ -19,16 +33,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -
     echo "Skipping deploy"
     exit 0
 fi
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 # Save some useful information
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
