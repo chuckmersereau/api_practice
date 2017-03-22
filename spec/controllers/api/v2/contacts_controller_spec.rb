5d6e01c970a7606c065743d0bf66e1e41d417fb4
Nathaniel Watts <reg@nathanielwatts.com>
2017-03-22 00:09:53 -0500

    MPDX-2364 & MPDX-2365 -- nested Referred_by_me creation w/ Account List & Account List ID validations on Contact (#264)
    * Add presence validation - Contact#account_list_id
    
    We don't ever want a Contact to be created without an `account_list_id`.
    Unfortunately, some were accidentally being created as such.
    
    This adds validation to help prevent that in the future.
    
    * Fix nested contacts_referred_by_me creation
    
    Previously - our allowed nested attributes for `contacts_referred_by_me`
    didn't include `uuid` or `account_list_id`, which prevented correct
    creation of those resources on the API.
    
    This adds those - and adds a test to validate that it is indeed
    possible.

--- spec/controllers/api/v2/contacts_controller_spec.rb
+++ spec/controllers/api/v2/contacts_controller_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe Api::V2::ContactsController, type: :controller do
-  let(:factory_type) { :contact }
-  let!(:user) { create(:user_with_account) }
-  let(:account_list) { user.account_lists.first }
+  let(:factory_type)    { :contact }
+  let!(:user)           { create(:user_with_account) }
+  let(:account_list)    { user.account_lists.first }
   let(:account_list_id) { account_list.uuid }
-  let(:contact) { create(:contact_with_person, account_list: account_list) }
+  let(:contact)         { create(:contact_with_person, account_list: account_list) }
   let!(:second_contact) { create(:contact, account_list: account_list) }
-  let(:id) { contact.uuid }
+  let(:id)              { contact.uuid }
 
   let!(:resource) { contact }
   let(:second_resource) { second_contact }
@@ -17,8 +17,7 @@ describe Api::V2::ContactsController, type: :controller do
       account_list: {
         data: {
           type: 'account_lists',
-          id: create(:account_list).uuid
-        }
+          id: create(:account_list).uuid }
       }
     }
   end
@@ -68,6 +67,70 @@ describe Api::V2::ContactsController, type: :controller do
         get :index, filter: { account_list_id: account_list_two.uuid }
         expect(response.status).to eq(200)
         expect(JSON.parse(response.body)['data'].length).to eq(1)
+      end
+    end
+  end
+
+  describe 'Nested Creating / Updating of Resources' do
+    describe 'Created a nested Referral with an Account List' do
+      let(:generated_uuid) { SecureRandom.uuid }
+
+      let(:params) do
+        {
+          included: [
+            {
+              type: 'contacts',
+              id: generated_uuid,
+              attributes: {
+                name: 'thompson, jim and jan',
+                primary_person_first_name: 'jim',
+                primary_person_last_name: 'thomson',
+                primary_person_email: 'jim.thompson@example.com',
+                primary_person_phone: '4074969081',
+                spouse_first_name: 'jan',
+                spouse_last_name: 'thompson'
+              },
+              relationships: {
+                account_list: {
+                  data: {
+                    type: 'account_lists',
+                    id: account_list_id
+                  }
+                }
+              }
+            }
+          ],
+          data: {
+            type: 'contacts',
+            id: id,
+            attributes: {
+              updated_in_db_at: Time.current
+            },
+            relationships: {
+              contacts_referred_by_me: {
+                data: [
+                  {
+                    type: 'contacts',
+                    id: generated_uuid
+                  }
+                ]
+              }
+            }
+          },
+          id: id
+        }
+      end
+
+      it 'creates the nested referral' do
+        expect(resource.contact_referrals_by_me.count).to eq(0)
+
+        api_login(user)
+        put :update, params
+
+        expect(resource.reload.contact_referrals_by_me.count).to          eq(1)
+        expect(resource.contacts_referred_by_me.first.name).to            eq 'thompson, jim and jan'
+        expect(resource.contacts_referred_by_me.first.account_list_id).to eq account_list.id
+        expect(resource.contacts_referred_by_me.first.uuid).to            eq generated_uuid
       end
     end
   end
