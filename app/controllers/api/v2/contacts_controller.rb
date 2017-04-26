f8f43d50ba11293c6a257dbcaf4f50c073e760aa
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-04-26 23:54:01 +1200

    remove associations that are method based

--- app/controllers/api/v2/contacts_controller.rb
+++ app/controllers/api/v2/contacts_controller.rb
@@ -2,7 +2,7 @@ class Api::V2::ContactsController < Api::V2Controller
   def index
     authorize_index
     load_contacts
-    render json: @contacts.preload(include_associations).preload(attributes_to_preload),
+    render json: @contacts.preload(include_associations(Contact)).preload(attributes_to_preload),
            meta: meta_hash(@contacts),
            include: include_params,
            fields: field_params
--- app/controllers/concerns/including.rb
+++ app/controllers/concerns/including.rb
@@ -8,7 +8,9 @@ module Including
     params[:include].split(',') - UNPERMITTED_INCLUDE_PARAMS
   end
 
-  def include_associations
-    ::JSONAPI::IncludeDirective.new(include_params - ['*']).to_hash
+  def include_associations(klass = nil)
+    include_associations = ::JSONAPI::IncludeDirective.new(include_params - ['*']).to_hash
+    return include_associations unless klass
+    include_associations.select { |association| klass.reflections.include?(association) }
   end
 end
