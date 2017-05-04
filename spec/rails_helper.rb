95a55e29c95b535b6007db5bc1baa11b54e70064
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-05-04 13:38:55 +1200

    Travis optimisations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,17 +1,19 @@
-require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter 'dev/'
-  add_filter 'vendor/'
-end
+if ENV['CI']
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    add_filter 'dev/'
+    add_filter 'vendor/'
+  end
 
-if ENV['COVERALLS_REPO_TOKEN']
-  require 'coveralls'
-  Coveralls.wear_merged!('rails')
-end
+  if ENV['COVERALLS_REPO_TOKEN']
+    require 'coveralls'
+    Coveralls.wear_merged!('rails')
+  end
 
-if ENV['CODECOV_TOKEN']
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  if ENV['CODECOV_TOKEN']
+    require 'codecov'
+    SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  end
 end
 
 ENV['RAILS_ENV'] ||= 'test'
