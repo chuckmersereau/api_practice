79ffff74720e34c587e291a9de879e47cb71317b
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-05-04 14:06:44 +1200

    configuration fixes

--- bin/travis_parallel_test.sh
+++ bin/travis_parallel_test.sh
@@ -8,9 +8,13 @@ elif [ "$TEST_SUITE" = "3" ]
 then
   TEST_GROUPS=5,6
 fi
+elif [ "$TEST_SUITE" = "4" ]
+then
+TEST_GROUPS=7,8
+fi
 
 if [ "$TEST_SUITE" != "5" ]
 then
-  DISABLE_SPRING=1 bundle exec parallel_test spec/ -n 6 \
+  DISABLE_SPRING=1 bundle exec parallel_test spec/ -n 8 \
     --only-group $TEST_GROUPS --group-by filesize --type rspec --verbose
 fi
