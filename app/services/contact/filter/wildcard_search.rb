910c5d85f65844d2fdfbfb6a95a840a5a8f0b0c1
Sheldon D <sheldond@users.noreply.github.com>
2017-05-29 21:30:22 -0400

    MPDX-3694 Search Bar: add ability to search first name and last name out of order

--- app/services/contact/filter/wildcard_search.rb
+++ app/services/contact/filter/wildcard_search.rb
@@ -1,51 +1,72 @@
 class Contact::Filter::WildcardSearch < Contact::Filter::Base
   def execute_query(contacts, filters)
-    search_term = filters[:wildcard_search].downcase
+    @search_term = filters[:wildcard_search].downcase
+    @contacts = contacts
 
-    contact_ids  = contact_ids_from_email_addresses_like_search_term(contacts, search_term)
-    contact_ids += contact_ids_from_phone_numbers_like_search_term(contacts, search_term)
-    contact_ids += contact_ids_from_donor_account_numbers_like_search_term(contacts, search_term)
-    contact_ids += contact_ids_from_people_with_first_or_last_name_like_search_term(contacts, search_term)
+    contact_ids  = contact_ids_from_email_addresses_like_search_term
+    contact_ids += contact_ids_from_phone_numbers_like_search_term
+    contact_ids += contact_ids_from_donor_account_numbers_like_search_term
+    contact_ids += contact_ids_from_people_with_first_or_last_name_like_search_term
 
-    contacts_where_name_or_notes_like_search_term_or_with_ids(contacts, search_term, contact_ids)
+    contacts_where_name_or_notes_like_search_term_or_with_ids(contact_ids)
   end
 
   private
 
-  def contact_ids_from_donor_account_numbers_like_search_term(contacts, search_term)
-    contacts
+  def contact_ids_from_donor_account_numbers_like_search_term
+    @contacts
       .joins(:donor_accounts)
-      .where('lower(donor_accounts.account_number) like :search', search: "#{search_term}%")
+      .where('donor_accounts.account_number ilike :search', query_params)
       .ids
   end
 
-  def contact_ids_from_email_addresses_like_search_term(contacts, search_term)
-    contacts
+  def contact_ids_from_email_addresses_like_search_term
+    @contacts
       .joins(people: :email_addresses)
-      .where('lower(email_addresses.email) like :search', search: "#{search_term}%")
+      .where('email_addresses.email ilike :search', query_params)
       .ids
   end
 
-  def contact_ids_from_people_with_first_or_last_name_like_search_term(contacts, search_term)
-    contacts
+  def contact_ids_from_people_with_first_or_last_name_like_search_term
+    @contacts
       .joins(:people)
-      .where('lower(people.first_name) like :search OR lower(people.last_name) like :search', search: "#{search_term}%")
+      .where(sql_condition_to_search_columns_in_parts('people.first_name', 'people.last_name'), query_params(search_term_parts_hash))
       .ids
   end
 
-  def contact_ids_from_phone_numbers_like_search_term(contacts, search_term)
-    contacts
+  def contact_ids_from_phone_numbers_like_search_term
+    @contacts
       .joins(people: :phone_numbers)
-      .where('lower(phone_numbers.number) like :search', search: "%#{search_term}%")
+      .where('phone_numbers.number ilike :search', query_params)
       .ids
   end
 
-  def contacts_where_name_or_notes_like_search_term_or_with_ids(contacts, search_term, contact_ids)
-    contacts
-      .where(
-        'lower(contacts.name) like :search OR lower(contacts.notes) like :search OR contacts.id IN (:contact_ids)',
-        search: "%#{search_term}%",
-        contact_ids: contact_ids
-      )
+  def contacts_where_name_or_notes_like_search_term_or_with_ids(contact_ids)
+    or_conditions = [
+      sql_condition_to_search_columns_in_parts('contacts.name'),
+      'contacts.notes ilike :search',
+      'contacts.id IN (:contact_ids)'
+    ].join(' OR ')
+
+    @contacts.where(or_conditions, query_params(search_term_parts_hash.merge(contact_ids: contact_ids)))
+  end
+
+  def sql_condition_to_search_columns_in_parts(*columns)
+    search_term_parts_hash.keys.collect do |part_key|
+      columns.collect do |column|
+        "#{column} ilike :#{part_key}"
+      end.join(' OR ').prepend('(') + ')'
+    end.flatten.join(' AND ').prepend('(') + ')'
+  end
+
+  def search_term_parts_hash
+    @search_term_parts_hash ||= @search_term.gsub(/[,-]/, ' ').split(' ').each_with_object({}) do |search_term_part, hash|
+      hash["search_part_#{hash.size}".to_sym] = "%#{search_term_part}%"
+      hash
+    end
+  end
+
+  def query_params(extra_params = {})
+    { search: "%#{@search_term}%" }.merge(extra_params)
   end
 end
--- spec/services/contact/filter/wildcard_search_spec.rb
+++ spec/services/contact/filter/wildcard_search_spec.rb
@@ -4,11 +4,15 @@ RSpec.describe Contact::Filter::WildcardSearch do
   let!(:user) { create(:user_with_account) }
   let!(:account_list) { user.account_lists.first }
 
-  let!(:person) { create(:person, email_addresses: [build(:email_address, email: 'email@gmail.com')], phone_numbers: [build(:phone_number, number: '514 122-4362')]) }
-  let!(:contact_one) { create(:contact, account_list: account_list, notes: 'the random notes', name: 'A name', donor_accounts: [build(:donor_account, account_number: 'ACCOUNT_NUMBER')]) }
-  let!(:contact_two) { create(:contact, account_list: account_list, notes: 'notes present') }
-  let!(:contact_three) { create(:contact, account_list: account_list, notes: 'missing present', people: [person]) }
-  let!(:contact_four) { create(:contact, account_list: account_list) }
+  let!(:person) do
+    create(:person, first_name: 'PersonFirstName', last_name: 'PersonLastName',
+                    email_addresses: [build(:email_address, email: 'email@gmail.com')],
+                    phone_numbers: [build(:phone_number, number: '514 122-4362')])
+  end
+  let!(:contact_one) { create(:contact, account_list: account_list, notes: 'the random notes', name: 'Jones, Freddie', donor_accounts: [build(:donor_account, account_number: '1234567890')]) }
+  let!(:contact_two) { create(:contact, account_list: account_list, notes: 'this is my favourite person', name: 'Dolly, Doe') }
+  let!(:contact_three) { create(:contact, account_list: account_list, notes: 'missing', name: 'Jill, Zhong', people: [person]) }
+  let!(:contact_four) { create(:contact, account_list: account_list, notes: nil, name: 'Bob, Bindal') }
 
   describe '#config' do
     it 'does not have config' do
@@ -29,13 +33,32 @@ RSpec.describe Contact::Filter::WildcardSearch do
     end
 
     context 'filter with wildcard search' do
-      it 'returns only tasks that are within the updated_at range' do
-        expect(described_class.query(contacts, { wildcard_search: 'A name' }, nil).to_a).to match_array [contact_one]
-        expect(described_class.query(contacts, { wildcard_search: 'ACCOUNT_NUMBER' }, nil).to_a).to match_array [contact_one]
+      it 'returns only contacts that match the search query' do
+        expect(described_class.query(contacts, { wildcard_search: 'Freddie' }, nil).to_a).to match_array [contact_one]
+        expect(described_class.query(contacts, { wildcard_search: '1234567890' }, nil).to_a).to match_array [contact_one]
         expect(described_class.query(contacts, { wildcard_search: person.last_name }, nil).to_a).to match_array [contact_three]
         expect(described_class.query(contacts, { wildcard_search: '122' }, nil).to_a).to match_array [contact_three]
         expect(described_class.query(contacts, { wildcard_search: 'random notes' }, nil).to_a).to match_array [contact_one]
         expect(described_class.query(contacts, { wildcard_search: 'email' }, nil).to_a).to match_array [contact_three]
+      end
+
+      it 'searches contact name regardless of order, case, or commas' do
+        expect(described_class.query(contacts, { wildcard_search: 'freddie JONES,' }, nil).to_a).to match_array [contact_one]
+        expect(described_class.query(contacts, { wildcard_search: 'jones, freddie' }, nil).to_a).to match_array [contact_one]
+      end
+
+      it 'searches person first and last name regardless of order, case, or commas' do
+        expect(described_class.query(contacts, { wildcard_search: 'Personfirstname, personlastName' }, nil).to_a).to match_array [contact_three]
+        expect(described_class.query(contacts, { wildcard_search: ',PERSONLASTNAME personfirstname' }, nil).to_a).to match_array [contact_three]
+      end
+
+      it 'searches names with more than two words' do
+        contact_one.update(name: 'Min jun, Park')
+        contact_two.update(name: 'Seo-yun, Kim')
+        expect(described_class.query(contacts, { wildcard_search: 'park min jun' }, nil).to_a).to match_array [contact_one]
+        expect(described_class.query(contacts, { wildcard_search: 'park min-jun' }, nil).to_a).to match_array [contact_one]
+        expect(described_class.query(contacts, { wildcard_search: 'seo yun kim' }, nil).to_a).to match_array [contact_two]
+        expect(described_class.query(contacts, { wildcard_search: 'kim seo-yun' }, nil).to_a).to match_array [contact_two]
       end
     end
   end
