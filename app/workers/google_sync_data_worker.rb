7364f890b752f1616c36b952f82c06165eae25e4
Sheldon D <sheldond@users.noreply.github.com>
2017-07-13 12:11:00 -0400

    Hotfix: Make GoogleSyncDataWorker jobs unique (#548)
    It turns out there are various events that can queue a lot of duplicate google sync jobs,
    for now we will rely on sidekiq-unique-jobs to prevent it.

--- app/workers/google_sync_data_worker.rb
+++ app/workers/google_sync_data_worker.rb
@@ -1,7 +1,7 @@
 class GoogleSyncDataWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :api_google_sync_data_worker
+  sidekiq_options queue: :api_google_sync_data_worker, unique: :until_executed
 
   def perform(google_integration_id, integration)
     GoogleIntegration.find(google_integration_id).sync_data(integration)
