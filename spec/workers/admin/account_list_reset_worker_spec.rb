247c6f7868a41f4668ee48d69f6ba7cd18ca2bb5
Sheldon D <sheldond@users.noreply.github.com>
2017-07-20 12:55:53 -0400

    MPDX-3871 If all of a user's account lists are reset, they should still have a default account list set

--- spec/workers/admin/account_list_reset_worker_spec.rb
+++ spec/workers/admin/account_list_reset_worker_spec.rb
@@ -8,73 +8,140 @@ describe Admin::AccountListResetWorker do
 
   before do
     create(:user_with_account)
+    user.update(default_account_list: account_list.id)
   end
 
-  describe '#perform' do
-    subject do
-      Admin::AccountListResetWorker.new.perform(account_list.id, user.id, reset_log.id)
+  subject do
+    Admin::AccountListResetWorker.new.perform(account_list.id, user.id, reset_log.id)
+  end
+
+  context 'user has multiple account lists' do
+    before do
+      create(:organization_account, person: user)
+      user.reload
+      expect(user.account_lists.size).to eq(2)
     end
 
-    it 'authorizes the reset' do
-      expect_any_instance_of(PunditAuthorizer).to receive(:authorize_on).with('create')
-      expect do
-        expect(subject).to eq(true)
-      end.to change { AccountList.count }.by(-1)
+    describe '#perform' do
+      it 'authorizes the reset' do
+        expect_any_instance_of(PunditAuthorizer).to receive(:authorize_on).with('create')
+        expect do
+          expect(subject).to eq(true)
+        end.to change { AccountList.count }.by(-1)
+      end
+
+      it 'does not destroy the account_list if the admin is not an admin' do
+        reset_log.update(admin_resetting: create(:user))
+        reset_log.reload
+        expect(AccountList::Destroyer).to_not receive(:new).with('create')
+        expect do
+          expect(subject).to eq(false)
+        end.to_not change { AccountList.count }
+      end
+
+      it 'continues with the reset if the account_list was already destroyed' do
+        account_list.unsafe_destroy
+        expect do
+          expect(subject).to eq(true)
+        end.to_not change { AccountList.count }
+      end
+
+      it 'does not reset if the user is not found' do
+        user.delete
+        expect do
+          expect(subject).to eq(false)
+        end.to_not change { AccountList.count }
+      end
+
+      it 'does not reset if the reset_log is not found' do
+        reset_log.delete
+        expect do
+          expect(subject).to eq(false)
+        end.to_not change { AccountList.count }
+      end
+
+      it 'deletes the account_list' do
+        account_list_id = account_list.id
+        expect do
+          expect(subject).to eq(true)
+        end.to change { AccountList.where(id: account_list_id).count }.from(1).to(0)
+          .and change { AccountList.count }.from(4).to(3)
+      end
+
+      it 'imports the profiles' do
+        organization_account_double = instance_double('Person::OrganizationAccount', queue_import_data: nil, import_profiles: nil)
+        expect_any_instance_of(User).to receive(:organization_accounts).twice.and_return([organization_account_double])
+        expect(organization_account_double).to receive(:import_profiles)
+        subject
+      end
+
+      it 'queues a data sync' do
+        organization_account_double = instance_double('Person::OrganizationAccount', queue_import_data: nil, import_profiles: nil)
+        expect_any_instance_of(User).to receive(:organization_accounts).twice.and_return([organization_account_double])
+        expect(organization_account_double).to receive(:queue_import_data)
+        subject
+      end
+
+      it 'logs the complete time' do
+        time = Time.current
+        travel_to time do
+          expect { subject }.to change { reset_log.reload.completed_at&.to_i }.from(nil).to(time.to_i)
+        end
+      end
+
+      it 'updates the default_account_list' do
+        expect { subject }.to change { user.reload.default_account_list }.from(account_list.id)
+        expect(user.account_lists.ids).to include(user.default_account_list)
+      end
+    end
+  end
+
+  context 'user has only one account list' do
+    before do
+      expect(user.account_lists.size).to eq(1)
     end
 
-    it 'does not destroy the account_list if the admin is not an admin' do
-      reset_log.update(admin_resetting: create(:user))
-      reset_log.reload
-      expect(AccountList::Destroyer).to_not receive(:new).with('create')
-      expect do
-        expect(subject).to eq(false)
-      end.to_not change { AccountList.count }
+    context 'user has no organization account' do
+      before do
+        user.organization_accounts.delete_all
+        user.reload
+        expect(user.organization_accounts.size).to eq(0)
+      end
+
+      it 'updates the default_account_list' do
+        expect { subject }.to change { user.reload.default_account_list }.from(account_list.id).to(nil)
+      end
     end
 
-    it 'continues with the reset if the account_list was already destroyed' do
-      account_list.unsafe_destroy
-      expect do
-        expect(subject).to eq(true)
-      end.to_not change { AccountList.count }
-    end
+    context 'user has an organization account' do
+      let(:org_account_double_class) do
+        Class.new do
+          def initialize(user)
+            @user = user
+          end
 
-    it 'does not reset if the user is not found' do
-      user.delete
-      expect do
-        expect(subject).to eq(false)
-      end.to_not change { AccountList.count }
-    end
+          def new_account_list
+            @new_account_list = FactoryGirl.create(:account_list)
+          end
 
-    it 'does not reset if the reset_log is not found' do
-      reset_log.delete
-      expect do
-        expect(subject).to eq(false)
-      end.to_not change { AccountList.count }
-    end
+          def import_profiles
+            @user.account_lists << @new_account_list
+          end
 
-    it 'deletes the account_list' do
-      account_list_id = account_list.id
-      expect do
-        expect(subject).to eq(true)
-      end.to change { AccountList.where(id: account_list_id).count }.from(1).to(0)
-        .and change { AccountList.count }.from(3).to(2)
-    end
+          def queue_import_data
+          end
+        end
+      end
+      let(:org_account_double) { org_account_double_class.new(user) }
+      let(:new_account_list) { org_account_double.new_account_list }
 
-    it 'imports the profiles' do
-      expect_any_instance_of(Person::OrganizationAccount).to receive(:import_profiles)
-      subject
-      expect(user.reload.designation_profiles).to be_blank
-    end
+      before do
+        expect(user.organization_accounts.size).to eq(1)
+        expect_any_instance_of(User).to receive(:organization_accounts).twice.and_return([org_account_double])
+      end
 
-    it 'queues a data sync' do
-      expect_any_instance_of(Person::OrganizationAccount).to receive(:queue_import_data)
-      subject
-    end
-
-    it 'logs the complete time' do
-      time = Time.current
-      travel_to time do
-        expect { subject }.to change { reset_log.reload.completed_at&.to_i }.from(nil).to(time.to_i)
+      it 'updates the default_account_list' do
+        expect { subject }.to change { user.reload.default_account_list }.from(account_list.id).to(new_account_list.id)
       end
     end
   end
