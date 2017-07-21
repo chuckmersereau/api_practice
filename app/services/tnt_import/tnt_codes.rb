6adfb6706d98347517f01adf972491ca5d67dce0
Sheldon D <sheldond@users.noreply.github.com>
2017-07-21 16:29:08 -0400

    MPDX-3966 Tnt does not import newsletter tasks

--- app/services/tnt_import/tnt_codes.rb
+++ app/services/tnt_import/tnt_codes.rb
@@ -25,20 +25,48 @@ class TntImport::TntCodes
     { field: 'SpouseCompanyMainPhone', location: 'work', person: :spouse } # index 18
   ].freeze
 
+  TNT_TASK_CODES_MAPPED_TO_MPDX_TASK_TYPES = {
+    1   => 'Appointment',
+    2   => 'Thank',
+    3   => 'To Do',
+    20  => 'Call',
+    30  => 'Reminder Letter',
+    40  => 'Support Letter',
+    50  => 'Letter',
+    60  => 'Newsletter - Physical',
+    65  => 'Newsletter - Email',
+    70  => 'Pre Call Letter',
+    100 => 'Email',
+    140 => 'Facebook Message',
+    150 => 'Text Message'
+  }.freeze
+
+  TNT_TASK_RESULT_CODES_MAPPED_TO_MPDX_TASK_RESULTS = {
+    1 => 'Done',
+    2 => 'Received',
+    3 => 'Attempted'
+  }.freeze
+
+  TNT_MPD_PHASE_CODES_MAPPED_TO_MPDX_CONTACT_STATUSES = {
+    0   => nil, # A "0" value for MPDPhaseID means "n/a" in Tnt
+    10  => 'Never Contacted',
+    20  => 'Ask in Future',
+    30  => 'Contact for Appointment',
+    40  => 'Appointment Scheduled',
+    50  => 'Call for Decision',
+    60  => 'Partner - Financial',
+    70  => 'Partner - Special',
+    80  => 'Partner - Pray',
+    90  => 'Not Interested',
+    95  => 'Unresponsive',
+    100 => 'Never Ask',
+    110 => 'Research Abandoned',
+    130 => 'Expired Referral'
+  }.freeze
+
   class << self
     def task_type(task_type_id)
-      case task_type_id.to_i
-      when 1 then 'Appointment'
-      when 2 then 'Thank'
-      when 3 then 'To Do'
-      when 20 then 'Call'
-      when 30 then 'Reminder Letter'
-      when 40 then 'Support Letter'
-      when 50 then 'Letter'
-      when 60 then 'Newsletter'
-      when 70 then 'Pre Call Letter'
-      when 100 then 'Email'
-      end
+      TNT_TASK_CODES_MAPPED_TO_MPDX_TASK_TYPES[task_type_id.to_i]
     end
 
     def task_status_completed?(task_status_id)
@@ -46,30 +74,11 @@ class TntImport::TntCodes
     end
 
     def history_result(history_result_id)
-      case history_result_id.to_i
-      when 1 then 'Done'
-      when 2 then 'Received'
-      when 3 then 'Attempted'
-      end
+      TNT_TASK_RESULT_CODES_MAPPED_TO_MPDX_TASK_RESULTS[history_result_id.to_i]
     end
 
     def mpd_phase(phase)
-      case phase.to_i
-      when 0  then nil # A "0" value for MPDPhaseID means "n/a" in Tnt
-      when 10 then 'Never Contacted'
-      when 20 then 'Ask in Future'
-      when 30 then 'Contact for Appointment'
-      when 40 then 'Appointment Scheduled'
-      when 50 then 'Call for Decision'
-      when 60 then 'Partner - Financial'
-      when 70 then 'Partner - Special'
-      when 80 then 'Partner - Pray'
-      when 90 then 'Not Interested'
-      when 95 then 'Unresponsive'
-      when 100 then 'Never Ask'
-      when 110 then 'Research Abandoned'
-      when 130 then 'Expired Referral'
-      end
+      TNT_MPD_PHASE_CODES_MAPPED_TO_MPDX_CONTACT_STATUSES[phase.to_i]
     end
   end
 end
--- spec/services/tnt_import/tnt_codes_spec.rb
+++ spec/services/tnt_import/tnt_codes_spec.rb
@@ -14,4 +14,46 @@ describe TntImport::TntCodes do
       end
     end
   end
+
+  describe '.task_type' do
+    it 'only includes types that are supported by MPDX' do
+      types = described_class::TNT_TASK_CODES_MAPPED_TO_MPDX_TASK_TYPES.keys.collect do |tnt_type_code|
+        described_class.task_type(tnt_type_code)
+      end
+      expect(types - Task::TASK_ACTIVITIES).to eq([])
+      expect(described_class::TNT_TASK_CODES_MAPPED_TO_MPDX_TASK_TYPES.values - Task::TASK_ACTIVITIES).to eq([])
+    end
+  end
+
+  describe '.task_status_completed?' do
+    it 'is true for 2' do
+      expect(described_class.task_status_completed?(2)).to eq(true)
+    end
+    it 'is false for other values' do
+      [0, 1, 3, nil, 'string'].each do |test_value|
+        expect(described_class.task_status_completed?(test_value)).to eq(false)
+      end
+    end
+  end
+
+  describe '.history_result' do
+    it 'only includes results that are supported by MPDX' do
+      results = described_class::TNT_TASK_RESULT_CODES_MAPPED_TO_MPDX_TASK_RESULTS.keys.collect do |tnt_result_code|
+        described_class.history_result(tnt_result_code)
+      end
+      acceptable_result_values = Task.all_result_options.values.flatten.uniq
+      expect(results - acceptable_result_values).to eq([])
+      expect(described_class::TNT_TASK_RESULT_CODES_MAPPED_TO_MPDX_TASK_RESULTS.values - acceptable_result_values).to eq([])
+    end
+  end
+
+  describe '.mpd_phase' do
+    it 'only includes statuses that are supported by MPDX, including nil' do
+      statuses = described_class::TNT_MPD_PHASE_CODES_MAPPED_TO_MPDX_CONTACT_STATUSES.keys.collect do |tnt_mpd_code|
+        described_class.mpd_phase(tnt_mpd_code)
+      end
+      expect(statuses - Contact::ASSIGNABLE_STATUSES).to eq([nil])
+      expect(described_class::TNT_MPD_PHASE_CODES_MAPPED_TO_MPDX_CONTACT_STATUSES.values - Contact::ASSIGNABLE_STATUSES).to eq([nil])
+    end
+  end
 end
