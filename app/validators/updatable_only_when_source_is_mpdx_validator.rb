4c4ee35d47c92e24f5d2a7b24fc0813e62206ddb
Daniel Fugere <daniel.fugere@ballistiq.com>
2017-07-21 11:36:59 -0700

    MPDX-3948_fix_addresses_with_source_manual (#564)

--- app/validators/updatable_only_when_source_is_mpdx_validator.rb
+++ app/validators/updatable_only_when_source_is_mpdx_validator.rb
@@ -1,6 +1,11 @@
 class UpdatableOnlyWhenSourceIsMpdxValidator < ActiveModel::EachValidator
+  # Since classic still needs to be supported, manual must be added to the list of valid sources,
+  # however this should be removed once we stop supporting classic.
+
+  VALID_SOURCES = %w(MPDX manual).freeze
+
   def validate_each(record, attribute, _value)
-    return if !record.persisted? || record.source == 'MPDX' || record.changes[attribute].blank?
+    return if !record.persisted? || VALID_SOURCES.include?(record.source) || record.changes[attribute].blank?
     record.errors[attribute] << 'cannot be changed because the source is not MPDX'
   end
 end
