1235461c9622a89463107310ee25521e3afd82d7
Daniel Fugere <daniel.fugere@ballistiq.com>
2017-07-26 15:44:26 -0700

    HOTFIX_export_to_appeal_list (#576)

--- app/workers/mail_chimp/export_contacts_worker.rb
+++ app/workers/mail_chimp/export_contacts_worker.rb
@@ -3,7 +3,9 @@ class MailChimp::ExportContactsWorker
   sidekiq_options queue: :api_mail_chimp_sync_worker, unique: :until_executed
 
   def perform(mail_chimp_account, list_id, contact_ids)
+    return if mail_chimp_account.primary_list_id == list_id
+
     MailChimp::Exporter.new(mail_chimp_account, list_id)
-                       .export_to_appeal_list(contact_ids)
+                       .export_contacts(contact_ids)
   end
 end
