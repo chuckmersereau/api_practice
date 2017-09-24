ebd680a979e4d5bd576a97b164a93f2d691822ec
Spencer Oberstadt <soberstadt@gmail.com>
2017-09-24 18:47:47 -0400

    remove api v1 controllers (#726)
    * remove v1 controllers
    
    * remove specs

--- app/controllers/api/v1/contacts/filters_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::Contacts::FiltersController < Api::V1::BaseController
-  def index
-    render json: { filters: Contact::Filterer.config([current_account_list]) }.to_json
-  end
-end
--- app/controllers/api/v1/contacts/referrals_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::Contacts::ReferralsController < Api::V1::BaseController
-  def index
-    contact = Contact.find(params[:id] || params[:contact_id])
-    return render json: contact.contact_referrals_by_me,
-                  each_serializer: ReferralsSerializer if contact
-    render nothing: true, status: 404
-  end
-end
