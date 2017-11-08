75d6ee65670aa6204bdc9656b4e630c3f678747c
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-11-08 21:23:57 +1300

    Revert "update pledge totalling (#837)"
    This reverts commit 294987d33945d8725d99fe3b63f8527c68c796ee.

--- app/services/account_list/pledges_total.rb
+++ app/services/account_list/pledges_total.rb
@@ -1,6 +1,4 @@
 class AccountList::PledgesTotal
-  attr_accessor :account_list, :contacts, :rates
-
   def initialize(account_list, contacts)
     @account_list = account_list
     @contacts = contacts
@@ -8,12 +6,16 @@ class AccountList::PledgesTotal
   end
 
   def total
-    total_usd = contacts.map(&method(:monthly_pledge_usd)).sum
-    (total_usd * default_currency_rate).round(2)
+    if $rollout.active?(:currencies, @account_list)
+      total_usd = @contacts.map(&method(:monthly_pledge_usd)).sum
+      (total_usd * default_currency_rate).round(2)
+    else
+      @contacts.to_a.sum(&:monthly_pledge).round(2)
+    end
   end
 
   def default_currency_rate
-    latest_rate(account_list.salary_currency_or_default)
+    latest_rate(@account_list.salary_currency)
   end
 
   def monthly_pledge_usd(partner)
@@ -22,7 +24,7 @@ class AccountList::PledgesTotal
 
   def latest_rate(currency)
     return 1 if currency == 'USD'
-    rates[currency] ||= find_latest_rate(currency)
+    @rates[currency] ||= find_latest_rate(currency)
   end
 
   def find_latest_rate(currency)
--- spec/services/account_list/pledges_total_spec.rb
+++ spec/services/account_list/pledges_total_spec.rb
@@ -3,47 +3,38 @@ require 'rails_helper'
 describe AccountList::PledgesTotal do
   let(:account_list) { create(:account_list, salary_organization_id: create(:organization).id) }
 
+  before do
+    allow($rollout).to receive(:active?) { true }
+  end
+
   it 'defaults unknown currency rates to 1' do
-    rate = described_class.new(account_list, account_list.contacts).latest_rate('ANY')
+    rate = AccountList::PledgesTotal.new(account_list, account_list.contacts).latest_rate('ANY')
     expect(rate).to eq(1)
   end
 
-  let!(:contact1) do
-    create(:contact,
-           account_list: account_list,
-           pledge_amount: 15,
-           pledge_currency: 'USD',
-           status: 'Partner - Financial')
-  end
-  let!(:contact2) do
-    create(:contact,
-           account_list: account_list,
-           pledge_amount: 10,
-           pledge_currency: 'USD',
-           status: 'Partner - Financial')
-  end
-
   it 'calculates total pledges with no Financial Partners' do
-    contact1.update(status: 'Partner - Special')
-    contact2.update(status: '')
+    create(:contact, account_list: account_list, pledge_amount: 15, pledge_currency: 'USD', status: 'Partner - Special')
+    create(:contact, account_list: account_list, pledge_amount: 10, pledge_currency: 'USD', status: '')
+
     expect(account_list.total_pledges).to eq(0)
   end
 
   it 'calculates total pledges' do
-    expect(account_list.total_pledges).to eq(25)
+    create(:contact, account_list: account_list, pledge_amount: 15, pledge_currency: 'USD', status: 'Partner - Financial')
+    create(:contact, account_list: account_list, pledge_amount: 10, pledge_currency: 'USD', status: '')
+
+    expect(account_list.total_pledges).to eq(15)
   end
 
-  context 'multi-currency' do
-    before do
-      account_list.update(currency: 'EUR')
-      contact2.update(pledge_amount: 95, pledge_currency: 'RUB')
-      create(:currency_rate, rate: 0.8, exchanged_on: '2016-02-15', code: 'EUR')
-      create(:currency_rate, rate: 0.75, exchanged_on: '2016-01-15', code: 'EUR')
-      create(:currency_rate, rate: 75.20, exchanged_on: '2016-02-15', code: 'RUB')
-    end
+  it 'calculates total pledges with currency conversion' do
+    account_list.update(currency: 'EUR')
+    create(:currency_rate, rate: 0.8, exchanged_on: '2016-02-15', code: 'EUR')
+    create(:currency_rate, rate: 0.75, exchanged_on: '2016-01-15', code: 'EUR')
+    create(:currency_rate, rate: 75.20, exchanged_on: '2016-02-15', code: 'RUB')
 
-    it 'calculates total pledges with currency conversion' do
-      expect(account_list.total_pledges).to eq(16.26)
-    end
+    create(:contact, account_list: account_list, pledge_amount: 15, pledge_currency: 'USD', status: 'Partner - Financial')
+    create(:contact, account_list: account_list, pledge_amount: 95, pledge_currency: 'RUB', status: 'Partner - Financial')
+
+    expect(account_list.total_pledges).to eq(16.26)
   end
 end
