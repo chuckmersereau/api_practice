65e641c1833521d010222b367d9a768e66968515
Spencer Oberstadt <soberstadt@gmail.com>
2017-11-17 08:13:16 -0500

    Fix stopped giving filter - Hotfix (#859)
    * failing spec
    
    * fix stopped_giving_range filter
    
    * shorten lines
    
    * rubocop

--- app/services/contact/filter/stopped_giving_range.rb
+++ app/services/contact/filter/stopped_giving_range.rb
@@ -1,17 +1,22 @@
-# We define stopped giving any contact with at least 3 gifts that gave his last donation within the time range.
+# We define stopped giving any contact with at least 3
+# gifts that gave his last donation within the time range.
 class Contact::Filter::StoppedGivingRange < Contact::Filter::Base
-  # If a donor stopped giving after 3 gifts we can assume that he actually gave regularly and then stopped
+  # If a donor stopped giving after 3 gifts we can assume
+  # that he actually gave regularly and then stopped
   PRIOR_NUMBER_OF_GIFTS = 3
 
   def execute_query(scope, filters)
     @contacts_that_have_stopped_giving = scope.where(last_donation_date: filters[:stopped_giving_range])
 
-    @last_ten_months = (filters[:stopped_giving_range].last - 10.months)..filters[:stopped_giving_range].last
-    @contacts_that_have_stopped_giving.where(id: ids_of_contacts_that_stopped_giving_and_have_more_than_three_donations)
+    end_date = filters[:stopped_giving_range].last
+    @last_ten_months = (end_date - 10.months)..end_date
+    @contacts_that_have_stopped_giving.where(id: stopped_giving_and_many_donations_contact_ids)
   end
 
   def valid_filters?(filters)
-    super && date_range?(filters[:stopped_giving_range]) && filters[:stopped_giving_range].last <= valid_end_date
+    super &&
+      date_range?(filters[:stopped_giving_range]) &&
+      filters[:stopped_giving_range].last <= valid_end_date
   end
 
   private
@@ -20,10 +25,13 @@ class Contact::Filter::StoppedGivingRange < Contact::Filter::Base
     1.month.ago + 1.day
   end
 
-  def ids_of_contacts_that_stopped_giving_and_have_more_than_three_donations
+  def stopped_giving_and_many_donations_contact_ids
+    conditions = {
+      contact_donor_accounts: { contact_id: @contacts_that_have_stopped_giving.pluck(:id) },
+      donations: { donation_date: @last_ten_months }
+    }
     DonorAccount.joins({ contact_donor_accounts: :contact }, :donations)
-                .where(contact_donor_accounts: { contact: @contacts_that_have_stopped_giving },
-                       donations: { donation_date: @last_ten_months })
+                .where(conditions)
                 .group('contacts.id')
                 .having('COUNT(*) >= ?', PRIOR_NUMBER_OF_GIFTS).pluck('contacts.id')
   end
