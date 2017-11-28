fccb767b1fc42d1b74a81948e737fab28ea1877c
Spencer Oberstadt <soberstadt@gmail.com>
2017-11-27 21:49:22 -0500

    MPDX-4447 Match tnt donations to appeals (#861)
    * match existing tnt donations to appeals
    
    * clean up code some
    
    * match donations based on donor/date/amount
    
    * scope down donations we are searching
    
    * dry 🏜
    
    * address review feedback
    
    * link donations based on tnt contact id
    
    * search by contact first before donor account
    
    * map updated donations in perform
    
    * log to fatal the changed donation ids

--- app/services/tnt_import/xml.rb
+++ app/services/tnt_import/xml.rb
@@ -17,6 +17,16 @@ class TntImport::Xml
     tables[name]
   end
 
+  # finds a row from table_name table matching either the id given, or the whole hash
+  def find(table_name, value)
+    return unless tables[table_name.to_s]
+    tables[table_name.to_s].find do |r|
+      next r.slice(*value.keys) == value if value.is_a? Hash
+
+      r['id'] == value
+    end
+  end
+
   private
 
   def parse_row(row)
