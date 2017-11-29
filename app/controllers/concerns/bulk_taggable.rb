1f703a6360c1bedcf7359fdef26d177dab084172
Jon Sangster <996850+sangster@users.noreply.github.com>
2017-11-28 23:38:30 -0800

    Gracefully handle unexpected param type (#870)

--- app/controllers/concerns/bulk_taggable.rb
+++ app/controllers/concerns/bulk_taggable.rb
@@ -8,7 +8,7 @@ module BulkTaggable
   def tag_names
     params
       .require(:data)
-      .collect { |hash| hash[:data][:attributes][:name] }
+      .collect(&method(:extract_tag_name))
   end
 
   def taggable_ids
@@ -23,5 +23,23 @@ module BulkTaggable
 
   def missing_id_error
     # This method is a no-op because we do not care about IDs for tags
+  end
+
+  def extract_tag_name(hash)
+    name = hash.dig(:data, :attributes, :name)
+    return name if name.is_a?(String)
+
+    type = type_name(name)
+    raise Exceptions::BadRequestError,
+          format('Expected tag name to be a string, but it was %s %s with a value of %p',
+                 type.indefinite_article, type, name)
+  end
+
+  def type_name(object)
+    if object.is_a?(Hash) || object.is_a?(ActionController::Parameters)
+      'object'
+    else
+      name.class.name.downcase
+    end
   end
 end
