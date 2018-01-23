8dd624356aee2db2a58cac872ae2e95caee06a3d
Jon Sangster <996850+sangster@users.noreply.github.com>
2018-01-23 09:22:03 -0800

    Protect ActsAsTaggableOn gem from backslashes (#920)
    * Protect ActsAsTaggableOn gem from backslashes
    
    * remove unneeded expectation

--- spec/services/contact/filter/exclude_tags_spec.rb
+++ spec/services/contact/filter/exclude_tags_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 RSpec.describe Contact::Filter::ExcludeTags do
   let!(:user) { create(:user_with_account) }
   let!(:account_list) { user.account_lists.first }
@@ -34,6 +36,16 @@ RSpec.describe Contact::Filter::ExcludeTags do
       end
       it 'accepts tags as comma separated string' do
         expect(described_class.query(contacts, { exclude_tags: 'tag1,tag2,tag3' }, nil).to_a).to match_array [contact_four]
+      end
+
+      context 'with a backslash' do
+        before { contact_four.update!(tag_list: 'foo\bar') }
+
+        it 'does not fail with tags that include backslashes' do
+          expected_array = [contact_one, contact_two, contact_three]
+
+          expect(described_class.query(contacts, { exclude_tags: 'foo\bar' }, nil).to_a).to match_array expected_array
+        end
       end
     end
   end
