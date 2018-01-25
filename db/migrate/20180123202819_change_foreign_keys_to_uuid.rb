25d37da2737be3347002459d5ac1d58be1a75f22
Spencer Oberstadt <soberstadt@gmail.com>
2018-01-25 13:24:53 -0500

    WIP - first draft migration - it actually worked!

---
+++ db/migrate/20180123202819_change_foreign_keys_to_uuid.rb
@@ -0,0 +1,108 @@
+class ChangeForeignKeysToUuid < ActiveRecord::Migration
+  def up
+    remove_foreign_keys
+
+    migrate_foreign_keys
+
+    convert_primary_keys_to_uuids
+
+    readd_foreign_keys
+  end
+
+  def id_to_uuid(table_name, relation_name, relation_klass)
+    relation_name = relation_name.sub(/_id$/, '')
+    foreign_key = "#{relation_name}_id".to_sym
+    new_foreign_key = "#{relation_name}_uuid".to_sym
+
+    add_column table_name, new_foreign_key, :uuid
+
+    # if foreign relation is polymorphic, we have to do some more complicated work.
+    if relation_klass == 'Poly'
+      poly_id_to_uuid(table_name, relation_name, foreign_key, new_foreign_key)
+    else
+      execute_foreign_key_load(foreign_key, new_foreign_key, relation_klass, table_name)
+    end
+
+    remove_column table_name, foreign_key
+    rename_column table_name, new_foreign_key, foreign_key
+  end
+
+  def poly_id_to_uuid(table_name, relation_name, foreign_key, new_foreign_key)
+    foreign_tables = ActiveRecord::Base.connection.execute("SELECT DISTINCT #{relation_name}_type as type from #{table_name}")
+    foreign_tables.each do |foreign_table_row|
+      execute_foreign_key_load(foreign_key, new_foreign_key, foreign_table_row['type'], table_name)
+    end
+  end
+
+  def execute_foreign_key_load(foreign_key, new_foreign_key, relation_klass, table_name)
+    relation_table = table_name(relation_klass)
+    query = "UPDATE #{table_name}
+             SET #{new_foreign_key} = #{relation_table}.uuid
+             FROM #{relation_table}
+             WHERE #{table_name}.#{foreign_key} = #{relation_table}.id"
+    p 'Loading foreign keys with:'
+    p query.gsub('\n', '').gsub('  ', ' ')
+    execute query
+  end
+
+  def table_name(string)
+    return 'addresses' if string == 'Addressable'
+    return Person.const_get(string).table_name if string.starts_with? 'Person::'
+    string.classify.constantize.table_name
+  end
+
+  def try_class(string)
+    return Person.const_get(string) if string.starts_with? 'Person::'
+    string.classify.constantize
+  rescue NameError
+    nil
+  end
+
+  private
+
+  def migrate_foreign_keys
+    foreign_keys = CSV.read(Rails.root.join('db','foriegn_key_to_class_map.csv'))
+
+    foreign_keys.each { |fk| id_to_uuid(*fk) }
+  end
+
+  def convert_primary_keys_to_uuids
+    tables_sql = "SELECT DISTINCT table_name
+                  FROM information_schema.columns
+                  WHERE table_schema = 'public'
+                  and column_name = 'uuid'
+                  and data_type = 'uuid'"
+    tables = ActiveRecord::Base.connection.execute(tables_sql).collect { |row| row['table_name'] }
+
+    tables.each do |table|
+      remove_column table, :id
+      execute "UPDATE #{table} SET uuid = uuid_generate_v4() WHERE uuid IS NULL;"
+      rename_column table, :uuid, :id
+      execute "ALTER TABLE #{table} ADD PRIMARY KEY (id);"
+    end
+  end
+
+  def remove_foreign_keys
+    remove_foreign_key :appeal_excluded_appeal_contacts, :contacts
+    remove_foreign_key :appeal_excluded_appeal_contacts, :appeals
+    remove_foreign_key :background_batch_requests, :background_batches
+    remove_foreign_key :background_batches, :users
+    remove_foreign_key :donation_amount_recommendations, :donor_accounts
+    remove_foreign_key :donation_amount_recommendations, :designation_accounts
+    remove_foreign_key :master_person_sources, :master_people
+    remove_foreign_key :notification_preferences, :users
+    remove_foreign_key :people, :master_people
+  end
+
+  def readd_foreign_keys
+    add_foreign_key :appeal_excluded_appeal_contacts, :contacts, dependent: :delete
+    add_foreign_key :appeal_excluded_appeal_contacts, :appeals, dependent: :delete
+    add_foreign_key :background_batch_requests, :background_batches
+    add_foreign_key :background_batches, :people, column: :user_id
+    add_foreign_key :donation_amount_recommendations, :donor_accounts, dependent: :nullify
+    add_foreign_key :donation_amount_recommendations, :designation_accounts, dependent: :nullify
+    add_foreign_key :master_person_sources, :master_people
+    add_foreign_key :notification_preferences, :people, dependent: :delete, column: :user_id
+    add_foreign_key :people, :master_people, dependent: :restrict
+  end
+end
