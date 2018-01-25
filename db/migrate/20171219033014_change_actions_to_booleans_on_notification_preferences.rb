27e09d745f73ffdcc58a69f8cb1ee4afef3c698c
Spencer Oberstadt <soberstadt@gmail.com>
2018-01-25 10:31:54 -0500

    delete duplicate notification preferences before migrating (#933)
    * delete duplicate notification preferences before migrating
    
    * uncomment code

--- db/migrate/20171219033014_change_actions_to_booleans_on_notification_preferences.rb
+++ db/migrate/20171219033014_change_actions_to_booleans_on_notification_preferences.rb
@@ -2,6 +2,7 @@ class ChangeActionsToBooleansOnNotificationPreferences < ActiveRecord::Migration
   def change
     add_column :notification_preferences, :email, :boolean, default: true
     add_column :notification_preferences, :task, :boolean, default: true
+    NotificationPreference.delete_duplicates
     NotificationPreference.find_each(&:convert_actions_array_to_booleans)
     AccountListUser.includes(:user).where.not(people: { id: nil }).find_each(&:duplicate_notification_preferences)
     remove_column :notification_preferences, :actions
@@ -19,6 +20,27 @@ class ChangeActionsToBooleansOnNotificationPreferences < ActiveRecord::Migration
         email: actions.include?('email'),
         task: actions.include?('task')
       )
+    end
+
+    def delete_duplicates
+      deleted = 0
+
+      account_lists_with_prefs = AccountList.includes(:notification_preferences).where.not(notification_preferences: { id: nil })
+
+      account_lists_with_prefs.find_each do |account_list|
+        # notification_preferences should be loaded in memory already
+        groups = account_list.notification_preferences.to_a.group_by(&:notification_type_id)
+
+        groups.each do |_key, group|
+          # is there only one preference for this account_list/notification_type combo?
+          next if group.count <= 1
+
+          group[1..-1].each(&:destroy!)
+          deleted += (group.count - 1)
+        end
+      end
+
+      puts "Deleted #{deleted} duplicate notification preferences"
     end
   end
 
