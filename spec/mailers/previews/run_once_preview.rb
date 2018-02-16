95351d92799fd37e9b4ed0586c286c55955e2295
Spencer Oberstadt <soberstadt@gmail.com>
2018-02-16 11:12:16 -0500

    update email copy for newsletter fix worker (#969)

--- spec/mailers/previews/run_once_preview.rb
+++ spec/mailers/previews/run_once_preview.rb
@@ -1,9 +1,9 @@
 class RunOncePreview < ActionMailer::Preview
   def fix_newsletter_status
-    RunOnceMailer.fix_newsletter_status('bill.bright@cru.org', -1, 'Staff Account')
+    RunOnceMailer.fix_newsletter_status('bill.bright@cru.org', 12, 'Staff Account')
   end
 
   def new_mailchimp_list
-    RunOnceMailer.new_mailchimp_list('bill.bright@cru.org', -1, 'Staff Account', 'https://us1.admin.mailchimp.com/lists/members/?id=1234')
+    RunOnceMailer.new_mailchimp_list('bill.bright@cru.org', 12, 'Staff Account', 'https://us1.admin.mailchimp.com/lists/members/?id=1234')
   end
 end
