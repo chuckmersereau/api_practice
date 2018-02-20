c35a82f1d939258fb432122cba88a45ac17fc0a6
Spencer Oberstadt <soberstadt@gmail.com>
2018-02-20 08:25:23 -0500

    fix NotificationPreferencesCopyUserSpecificVersionsWorker (#975)

--- app/workers/run_once/notification_preferences_copy_user_specific_versions_worker.rb
+++ app/workers/run_once/notification_preferences_copy_user_specific_versions_worker.rb
@@ -16,7 +16,7 @@ class NotificationPreferencesCopyUserSpecificVersionsWorker
     account_list_user.account_list.notification_preferences.where(user_id: nil).find_each do |notification_preference|
       notification_preference.dup.tap do |user_notification_preference|
         user_notification_preference.uuid = nil
-        user_notification_preference.user = account_list_user.user
+        user_notification_preference.user_id = account_list_user.user_id
         user_notification_preference.save!
       end
     end
