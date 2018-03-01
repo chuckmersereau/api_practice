3fa9595133ca830fb98d9eb5704f23626c45e406
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-03-02 11:24:28 +1300

    kirby test fixes

--- spec/services/row_transfer_request_spec.rb
+++ spec/services/row_transfer_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe RowTransferRequest do
   describe 'transfer' do
     let!(:mock) do
-      stub_request(:post, 'http://example.com/')
+      stub_request(:post, ENV.fetch('KIRBY_URL'))
     end
     let(:ids) { %w(fc691be1-f667-46a5-bdbd-5b2751d98a90 d0be514b-d59f-4b57-9344-97317ab26c11) }
     let(:table_name) { 'people' }
