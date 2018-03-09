ad31135c60b6cf09328501708395f32cd2172572
Spencer Oberstadt <soberstadt@gmail.com>
2018-03-09 08:13:21 -0500

    skip sending emails if no email address (#1022)

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,8 @@ class NotificationMailer < ApplicationMailer
   def notify(user, notifications_by_type)
     @notifications_by_type = notifications_by_type
     @user = user
-    mail to: user.email.email, subject: _('Notifications from MPDX')
+    email = user&.email&.email
+    return unless email
+    mail to: email, subject: _('Notifications from MPDX')
   end
 end
