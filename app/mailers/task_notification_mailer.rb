1b03cfb1e8daec97c5ba1067d17efbba2e97408f
Spencer Oberstadt <soberstadt@gmail.com>
2018-03-15 09:23:43 -0400

    add time formate to en-US locale config (#1033)

--- app/mailers/task_notification_mailer.rb
+++ app/mailers/task_notification_mailer.rb
@@ -6,6 +6,7 @@ class TaskNotificationMailer < ApplicationMailer
     @user = User.find_by(id: user_id)
     email = @user&.email&.email
     return unless @task && email
+    I18n.locale = @user.locale || 'en'
     Time.use_zone(@user.time_zone) do
       mail to: email, subject: _('Task on MPDX')
     end
