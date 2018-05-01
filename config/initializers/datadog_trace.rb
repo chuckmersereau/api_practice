88df6d6111f5889d79efdba136a54ba1248df37d
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-05-01 13:39:41 -0400

    update datadog configuration (#1122)

--- config/initializers/datadog_trace.rb
+++ config/initializers/datadog_trace.rb
@@ -1,20 +1,25 @@
 base_name = ENV['PROJECT_NAME']
 enabled = ENV['DATADOG_TRACE'].to_s == 'true'
 
-Rails.configuration.datadog_trace = {
-  enabled: enabled,
-  auto_instrument: true,
-  auto_instrument_redis: true,
-  auto_instrument_grape: false,
-  default_service: base_name,
-  default_controller_service: "#{base_name}-controller",
-  default_cache_service: 'rails-cache',
-  default_database_service: "#{base_name}-db",
-  template_base_path: 'views/',
-  tracer: Datadog.tracer,
-  debug: false,
-  trace_agent_hostname: 'datadog-apm.aws.cru.org',
-  trace_agent_port: 8126,
-  env: Rails.env,
-  tags: {app: base_name }
-}
+Datadog.configure do |c|
+  # Tracer
+  c.tracer hostname: 'datadog-apm.aws.cru.org',
+           port: 8126,
+           tags: { app: base_name },
+           debug: false,
+           enabled: enabled,
+           env: Rails.env
+
+  # Rails
+  c.use :rails,
+        service_name: base_name,
+        controller_service: "#{base_name}-controller",
+        cache_service: "#{base_name}-cache",
+        database_service: "#{base_name}-db"
+
+  # Redis
+  c.use :redis, service_name: "#{base_name}-redis"
+
+  # Sidekiq
+  c.use :sidekiq, service_name: "#{base_name}-sidekiq"
+end
