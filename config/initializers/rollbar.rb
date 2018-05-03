2c65e61ef1d14986f790032efc7af916909c20a7
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-05-03 09:49:26 -0400

    use ROLLBAR_SIDEKIQ_TOKEN if running in sidekiq (#1121)

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,7 +2,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV['ROLLBAR_TOKEN']
+  config.access_token = Sidekiq.server? ? ENV['ROLLBAR_SIDEKIQ_TOKEN'] : ENV['ROLLBAR_TOKEN']
 
   # Here we'll disable in 'test' and 'development':
   enabled = !(Rails.env.test? || Rails.env.development?)
