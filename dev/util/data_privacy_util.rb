601d001594f3f8e765a6994ad73714210d8fbfe4
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-06-26 13:34:24 +1200

    Data privacy dev util (#1192)
    * add in a way to easily clear PII
    
    * destroy people
    
    * remove referral join objects
    
    * delete related tags
    
    * include logging
    
    * run everything in a transaction

--- dev/util/data_privacy_util.rb
+++ dev/util/data_privacy_util.rb
@@ -10,34 +10,36 @@ def anonymize_contacts_by_email(emails)
   confirm = $stdin.gets.chomp.upcase
   return unless confirm == 'Y'
   contacts.find_each do |contact|
-    new_contact_name = "DataPrivacy, Deleted #{Date.today.strftime('%Y/%m/%d')}"
-    contact.taggings.destroy_all
-    contact.tasks.destroy_all
-    contact.addresses.destroy_all
-    contact.people.destroy_all
-    contact.contact_referrals_to_me.destroy_all
-    contact.contact_referrals_by_me.destroy_all
-    contact.donor_accounts.update_all(
-      name: new_contact_name
-    )
-    contact.update(
-      name: new_contact_name,
-      pledge_amount: nil,
-      status: 'Never Ask',
-      notes: '',
-      full_name: new_contact_name,
-      greeting: '',
-      website: '',
-      church_name: '',
-      send_newsletter: nil,
-      timezone: nil,
-      envelope_greeting: '',
-      pledge_currency_code: nil,
-      pledge_currency: nil,
-      pledge_frequency: 0,
-      likely_to_give: nil,
-      locale: nil
-    )
+    ActiveRecord::Base.transaction do
+      new_contact_name = "DataPrivacy, Deleted #{Date.today.strftime('%Y/%m/%d')}"
+      contact.taggings.destroy_all
+      contact.tasks.destroy_all
+      contact.addresses.destroy_all
+      contact.people.destroy_all
+      contact.contact_referrals_to_me.destroy_all
+      contact.contact_referrals_by_me.destroy_all
+      contact.donor_accounts.update_all(
+        name: new_contact_name
+      )
+      contact.update(
+        name: new_contact_name,
+        pledge_amount: nil,
+        status: 'Never Ask',
+        notes: '',
+        full_name: new_contact_name,
+        greeting: '',
+        website: '',
+        church_name: '',
+        send_newsletter: nil,
+        timezone: nil,
+        envelope_greeting: '',
+        pledge_currency_code: nil,
+        pledge_currency: nil,
+        pledge_frequency: 0,
+        likely_to_give: nil,
+        locale: nil
+      )
+    end
     Rollbar.scope(contact: { id: contact.id }).info("PII Removal Completed for #{contact.id}")
     p "COMPLETED PII Removal for #{contact.id}"
   end
