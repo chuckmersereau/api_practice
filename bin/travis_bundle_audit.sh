90f2527b4d1ebabc0696c44aeea049b68aad6733
Spencer Oberstadt <soberstadt@gmail.com>
2018-08-27 19:25:30 -0400

    ignore CVE-2018-1000544 from bundle audit (#1255)

--- bin/travis_bundle_audit.sh
+++ bin/travis_bundle_audit.sh
@@ -1,4 +1,4 @@
 if [ "$TEST_SUITE" = "2" ]
 then
-  gem install bundler-audit && bundle audit check --update
+  gem install bundler-audit && bundle audit check --update --ignore CVE-2018-1000544
 fi
